b579a6b6cc562ce3a9c0376e40ff30bc729bfabb
Zaki Manian <zaki@manian.org>
2018-04-26 22:58:29 -0700

    Some encoding progress

--- src/types/heartbeat.rs
+++ src/types/heartbeat.rs
@@ -1,8 +1,9 @@
 
 use signatory::ed25519::Signature;
 use super::{ValidatorAddress,TendermintSign};
-use amino::Amino;
+use amino::*;
 use hex::encode;
+use bytes::BufMut;
 
 pub struct Heartbeat{
     validator_address: ValidatorAddress,
@@ -10,7 +11,7 @@ pub struct Heartbeat{
     height: i64,
     round: i64,
     sequence:i64,
-    Signature:Option<Signature>,
+    signature:Option<Signature>,
 }
 
 impl TendermintSign for Heartbeat{
@@ -30,10 +31,70 @@ impl TendermintSign for Heartbeat{
 
 impl Amino for Heartbeat{
     fn serialize(self)->Vec<u8>{
-        unimplemented!()
+        let mut buf =  vec![];
+
+        let  (_, mut pre) = compute_disfix("tendermint/socketpv/SignHeartbeatMsg");
+
+        pre[3] |= typ3_to_byte(Typ3Byte::Typ3_Struct); 
+
+        buf.put_slice(pre.as_slice());
+
+//Encode the Validator Address
+        {
+            {
+                let field_prefix = 1 << 3 | typ3_to_byte(Typ3Byte::Typ3_Struct); 
+                buf.put(field_prefix);
+            }
+            {
+                let field_prefix = 1 << 3 | typ3_to_byte(Typ3Byte::Typ3_ByteLength); 
+                buf.put(field_prefix);
+            }
+            encode_varint(20, &mut buf);
+
+            amino_bytes::encode(&self.validator_address.0, &mut buf);
+        }
+//Encode the validator index        
+        {
+            let field_prefix = 2 << 3 |typ3_to_byte(Typ3Byte::Typ3_Varint);
+            buf.put(field_prefix);
+            encode_varint(self.validator_index as u64, &mut buf);
+        }
+        {
+            let field_prefix = 3 << 3 |typ3_to_byte(Typ3Byte::Typ3_8Byte);
+            buf.put(field_prefix);
+            encode_int64(self.height as u64, &mut buf);
+        }
+        {
+            let field_prefix = 4 << 3 |typ3_to_byte(Typ3Byte::Typ3_Varint);
+            buf.put(field_prefix);
+            encode_varint(self.round as u64, &mut buf);
+        }
+        {
+            let field_prefix = 5 << 3 |typ3_to_byte(Typ3Byte::Typ3_Varint);
+            buf.put(field_prefix);
+            encode_varint(self.sequence as u64, &mut buf);
+        }
+
+        println!("{:x?}",buf );
+
+
+        buf
     }
 
     fn deserialize(self, data: &[u8]){
         unimplemented!()
     }
 }    
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_serialization() {
+        let addr:[u8;20] =[0xa3, 0xb2, 0xcc, 0xdd, 0x71, 0x86, 0xf1, 0x68, 0x5f, 0x21, 0xf2, 0x48, 0x2a, 0xf4, 0xfb, 0x34, 0x46, 0xa8, 0x4b, 0x35];
+        let heartbeat = Heartbeat{ validator_address:ValidatorAddress(addr), validator_index:1, height: 15, round: 10, sequence: 30, signature:None };
+        let _ = heartbeat.serialize();
+
+    }
+}
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -7,12 +7,8 @@ mod vote;
 pub struct ValidatorAddress([u8;20]);
 
 pub trait TendermintSign{
-    fn cannonicalize(self)->String;
+    fn cannonicalize(self, chain_id:&str)->String;
 }
 
-pub trait Amino{
-    fn serialize(self)->Vec<u8>;
-    fn deserialize(self, Vec<u8>);
-}
 
 pub use self::heartbeat::Heartbeat;
