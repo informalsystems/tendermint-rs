15bec914ed42e0f68ce0c1d203ad18079941f63e
Tony Arcieri <tony@iqlusion.io>
2018-07-31 07:00:48 -0700

    Upgrade to signatory 0.6.0-alpha1

--- src/ed25519/signer/dalek.rs
+++ src/ed25519/signer/dalek.rs
@@ -2,8 +2,8 @@
 //!
 //! This is mainly intended for testing/CI. Ideally real validators will use HSMs
 
-use signatory::ed25519::FromSeed;
-use signatory::providers::dalek::Ed25519Signer as DalekSigner;
+use signatory::ed25519::{self, FromSeed};
+use signatory::providers::dalek;
 use std::fs::File;
 use std::io::Read;
 
@@ -34,7 +34,8 @@ pub fn create_signers(signers: &mut Vec<Signer>, config: Option<DalekConfig>) ->
         let mut key_material = ClearOnDrop::new(vec![]);
         file.read_to_end(key_material.as_mut())?;
 
-        let signer = Box::new(DalekSigner::from_seed(&key_material).unwrap());
+        let seed = ed25519::Seed::from_slice(&key_material).unwrap();
+        let signer = Box::new(dalek::Ed25519Signer::from_seed(seed));
         signers.push(Signer::new(DALEK_PROVIDER_LABEL, key_id, signer));
     }
 
