03082551bc34783f687c444e2d05e73cbda24ec3
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2018-11-26 16:25:42 +0100

    tests for yubihsm subcommands

--- tests/cli/yubihsm/keys/generate.rs
+++ tests/cli/yubihsm/keys/generate.rs
@@ -1,2 +1,24 @@
 //! Integration tests for the `yubihsm keys generate` subcommand
-// TODO: `yubihsm keys generate` tests
+
+use cli;
+
+#[test]
+fn keys_generate_command_test() {
+    #[allow(unused_mut)]
+    let mut args = vec!["yubihsm", "keys", "generate", "1"];
+
+    #[cfg(feature = "yubihsm-mock")]
+    args.extend_from_slice(&["-c", super::KMS_CONFIG_PATH]);
+
+    let out = cli::run_successfully(args.as_slice());
+
+    assert_eq!(true, out.status.success());
+    assert_eq!(true, out.stderr.is_empty());
+    assert_eq!(
+        true,
+        String::from_utf8(out.stdout)
+            .unwrap()
+            .trim_start()
+            .starts_with("Generated key #1:")
+    );
+}
--- tests/cli/yubihsm/keys/import.rs
+++ tests/cli/yubihsm/keys/import.rs
@@ -1,2 +1,25 @@
 //! Integration tests for the `yubihsm keys import` subcommand
-// TODO: `yubihsm keys import` tests
+
+use cli;
+
+#[test]
+fn keys_import_command_test() {
+    #[allow(unused_mut)]
+    let mut args = vec!["yubihsm", "keys", "import", "1"];
+
+    #[cfg(feature = "yubihsm-mock")]
+    args.extend_from_slice(&["-c", super::KMS_CONFIG_PATH]);
+
+    let out = cli::run_successfully(args.as_slice());
+
+    assert_eq!(true, out.status.success());
+    assert_eq!(true, out.stderr.is_empty());
+    assert_eq!(
+        true,
+        String::from_utf8(out.stdout)
+            .unwrap()
+            .trim_start()
+            .starts_with("Generated key #1:")
+    );
+}
+
