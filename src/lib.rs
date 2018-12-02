8554a08422e4f09527f684feae213f8c2c4a493f
Tony Arcieri <tony@iqlusion.io>
2018-12-02 14:39:12 -0800

    tendermint.rs v0.1.4

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@
 #![forbid(unsafe_code)]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tendermint/kms/master/img/tendermint.png",
-    html_root_url = "https://docs.rs/tendermint/0.1.3"
+    html_root_url = "https://docs.rs/tendermint/0.1.4"
 )]
 
 #[cfg(feature = "secret-connection")]
