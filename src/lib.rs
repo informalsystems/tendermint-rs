1a32e6216420259384006d40c8d137025f551379
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2019-01-18 15:23:52 +0100

    tendermint-rs: bump version and update relevant files for release

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@
 #![forbid(unsafe_code)]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tendermint/kms/master/img/tendermint.png",
-    html_root_url = "https://docs.rs/tendermint/0.1.4"
+    html_root_url = "https://docs.rs/tendermint/0.1.5"
 )]
 
 #[cfg(feature = "secret-connection")]
