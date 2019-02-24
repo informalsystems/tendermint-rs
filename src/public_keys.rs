f7d815f43c61770470ec8815157f84316781c4bb
Zaki Manian <zakimanian@Zakis-MacBook-Pro.local>
2019-02-23 22:02:30 -0800

    Bump signatory to 11.1
    Support secp256k1 keys in tendermint-rs
    
    Support Account and Consensus Keys

--- src/public_keys.rs
+++ src/public_keys.rs
@@ -3,21 +3,35 @@
 
 use crate::error::Error;
 use sha2::{Digest, Sha256};
-use signatory::ed25519;
+use signatory::{ecdsa::curve::secp256k1, ed25519};
 use std::fmt::{self, Display};
 use subtle_encoding::bech32;
 
 /// Validator signing keys used for authenticating consensus protocol messages
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
-pub enum ConsensusKey {
+pub enum TendermintKey {
     /// Ed25519 consensus keys
     Ed25519(ed25519::PublicKey),
+    /// Secp256k1 consensus keys
+    Secp256k1(secp256k1::PublicKey),
 }
 
-impl ConsensusKey {
-    /// From raw Ed25519 public key bytes
-    pub fn from_raw_ed25519(bytes: &[u8]) -> Result<ConsensusKey, Error> {
-        Ok(ConsensusKey::Ed25519(ed25519::PublicKey::from_bytes(
+/// Validator signing keys used for authenticating consensus protocol messages
+pub struct ConsensusKey(TendermintKey);
+/// User signing keys used for interacting with accounts in the state machine
+pub struct AccountKey(TendermintKey);
+
+impl TendermintKey {
+    /// From raw secp256k1 public key bytes 
+    pub fn from_raw_secp256k1(bytes: &[u8]) -> Result<TendermintKey, Error>{
+        Ok(TendermintKey::Secp256k1(secp256k1::PublicKey::from_bytes(
+            bytes,
+        )?))
+    }
+
+    /// From raw Ed25519 public key bytes 
+    pub fn from_raw_ed25519(bytes: &[u8]) -> Result<TendermintKey, Error> {
+        Ok(TendermintKey::Ed25519(ed25519::PublicKey::from_bytes(
             bytes,
         )?))
     }
@@ -25,27 +39,83 @@ impl ConsensusKey {
     /// Get Ed25519 public key
     pub fn ed25519(self) -> Option<ed25519::PublicKey> {
         match self {
-            ConsensusKey::Ed25519(pk) => Some(pk),
+            TendermintKey::Ed25519(pk) => Some(pk),
+            _ => None,
         }
     }
 }
 
 impl Display for ConsensusKey {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        //Amino prefix for Pubkey
-        let mut key_bytes: Vec<u8> = vec![0x16, 0x24, 0xDE, 0x64, 0x20];
-        match self {
-            ConsensusKey::Ed25519(ref pk) => {
+        let key_bytes: Vec<u8> = match self.0 {
+            TendermintKey::Ed25519(ref pk) => {
+                //Amino prefix for Pubkey
+                let mut key_bytes = vec![0x16, 0x24, 0xDE, 0x64, 0x20];
                 key_bytes.extend(pk.as_bytes());
-                bech32::encode("cosmosvalconspub", &key_bytes).fmt(f)
+                key_bytes
             }
-        }
+            TendermintKey::Secp256k1(ref pk) => {
+                let mut key_bytes = vec![0xEB, 0x5A, 0xE9, 0x87, 0x21];
+                key_bytes.extend(pk.as_bytes());
+                key_bytes
+            }
+        };
+        bech32::encode("cosmosvalconspub", &key_bytes).fmt(f)
+    }
+}
+
+impl Display for AccountKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let key_bytes: Vec<u8> = match self.0 {
+            TendermintKey::Ed25519(ref pk) => {
+                //Amino prefix for Pubkey
+                let mut key_bytes = vec![0x16, 0x24, 0xDE, 0x64, 0x20];
+                key_bytes.extend(pk.as_bytes());
+                key_bytes
+            }
+            TendermintKey::Secp256k1(ref pk) => {
+                let mut key_bytes = vec![0xEB, 0x5A, 0xE9, 0x87, 0x21];
+                key_bytes.extend(pk.as_bytes());
+                key_bytes
+            }
+        };
+        bech32::encode("cosmospub", &key_bytes).fmt(f)
+    }
+}
+
+impl From<ed25519::PublicKey> for TendermintKey {
+    fn from(pk: ed25519::PublicKey) -> TendermintKey {
+        TendermintKey::Ed25519(pk)
+    }
+}
+
+impl From<secp256k1::PublicKey> for TendermintKey {
+    fn from(pk: secp256k1::PublicKey) -> TendermintKey {
+        TendermintKey::Secp256k1(pk)
     }
 }
 
 impl From<ed25519::PublicKey> for ConsensusKey {
     fn from(pk: ed25519::PublicKey) -> ConsensusKey {
-        ConsensusKey::Ed25519(pk)
+        ConsensusKey(TendermintKey::Ed25519(pk))
+    }
+}
+
+impl From<secp256k1::PublicKey> for ConsensusKey {
+    fn from(pk: secp256k1::PublicKey) -> ConsensusKey {
+        ConsensusKey(TendermintKey::Secp256k1(pk))
+    }
+}
+
+impl From<ed25519::PublicKey> for AccountKey {
+    fn from(pk: ed25519::PublicKey) -> AccountKey {
+        AccountKey(TendermintKey::Ed25519(pk))
+    }
+}
+
+impl From<secp256k1::PublicKey> for AccountKey {
+    fn from(pk: secp256k1::PublicKey) -> AccountKey {
+        AccountKey(TendermintKey::Secp256k1(pk))
     }
 }
 
@@ -93,7 +163,7 @@ impl From<ed25519::PublicKey> for SecretConnectionKey {
 
 #[cfg(test)]
 mod tests {
-    use super::{ConsensusKey, SecretConnectionKey};
+    use super::{SecretConnectionKey, TendermintKey, ConsensusKey, AccountKey};
     use subtle_encoding::hex;
 
     const EXAMPLE_SECRET_CONN_KEY: &str =
@@ -118,12 +188,26 @@ mod tests {
     #[test]
     fn test_consensus_serialization() {
         let example_key =
-            ConsensusKey::from_raw_ed25519(&hex::decode_upper(EXAMPLE_CONSENSUS_KEY).unwrap())
-                .unwrap();
+            ConsensusKey(TendermintKey::from_raw_ed25519(&hex::decode_upper(EXAMPLE_CONSENSUS_KEY).unwrap())
+                .unwrap());
 
         assert_eq!(
             example_key.to_string(),
             "cosmosvalconspub1zcjduepqfgjuveq2raetnjt4xwpffm63kmguxv2chdhvhf5lhslmtgeunh8qmf7exk"
+        );
+    }
+
+    const EXAMPLE_ACCOUNT_KEY: &str =
+        "02A1633CAFCC01EBFB6D78E39F687A1F0995C62FC95F51EAD10A02EE0BE551B5DC";
+        #[test]
+        fn test_account_serialization() {
+        let example_key =
+            AccountKey(TendermintKey::from_raw_secp256k1(&hex::decode_upper(EXAMPLE_ACCOUNT_KEY).unwrap())
+                .unwrap());
+
+        assert_eq!(
+            example_key.to_string(),
+            "cosmospub1addwnpepq2skx090esq7h7md0r3e76r6ruyet330e904r6k3pgpwuzl92x6actrt4uq"
         );
     }
 }
