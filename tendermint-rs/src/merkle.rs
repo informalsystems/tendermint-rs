352c1e2fc6c7e3cfe9c8c68ebc65ccaa92787dda
Ethan Buchman <ethan@coinculture.info>
2019-07-15 20:21:12 -0400

    tests and fixes

--- tendermint-rs/src/merkle.rs
+++ tendermint-rs/src/merkle.rs
@@ -33,8 +33,9 @@ fn get_split_point(length: usize) -> usize {
 // tmhash(0x00 || leaf)
 fn leaf_hash(bytes: &[u8]) -> [u8; HASH_SIZE] {
     // make a new array starting with 0 and copy in the bytes
-    let mut leaf_bytes = [0u8; HASH_SIZE + 1];
-    leaf_bytes[1..].copy_from_slice(bytes);
+    let mut leaf_bytes = Vec::with_capacity(bytes.len() + 1);
+    leaf_bytes.push(0x00);
+    leaf_bytes.extend_from_slice(bytes);
 
     // hash it !
     let digest = Sha256::digest(&leaf_bytes);
@@ -48,10 +49,10 @@ fn leaf_hash(bytes: &[u8]) -> [u8; HASH_SIZE] {
 // tmhash(0x01 || left || right)
 fn inner_hash(left: &[u8], right: &[u8]) -> [u8; HASH_SIZE] {
     // make a new array starting with 0x1 and copy in the bytes
-    let mut inner_bytes = [0u8; HASH_SIZE * 2 + 1];
-    inner_bytes[0] = 0x01;
-    inner_bytes[1..].copy_from_slice(left);
-    inner_bytes[HASH_SIZE + 1..].copy_from_slice(right);
+    let mut inner_bytes = Vec::with_capacity(left.len() + right.len() + 1);
+    inner_bytes.push(0x01);
+    inner_bytes.extend_from_slice(left);
+    inner_bytes.extend_from_slice(right);
 
     // hash it !
     let digest = Sha256::digest(&inner_bytes);
@@ -65,6 +66,7 @@ fn inner_hash(left: &[u8], right: &[u8]) -> [u8; HASH_SIZE] {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use subtle_encoding::hex; // TODO: use non-subtle ?
 
     #[test]
     fn test_get_split_point() {
@@ -81,5 +83,33 @@ mod tests {
     }
 
     #[test]
-    fn test_empty_leaf() {}
+    fn test_RFC6962_empty_leaf() {
+        let empty_leaf_root_hex = "6e340b9cffb37a989ca544e6bb780a2c78901d3fb33738768511a30617afa01d";
+        let empty_leaf_root = &hex::decode(empty_leaf_root_hex).unwrap();
+        let empty_tree: &[&[u8]] = &[&[]];
+        let root = simple_hash_from_byte_slices(empty_tree);
+        assert_eq!(empty_leaf_root, &root);
+    }
+
+    #[test]
+    fn test_RFC6962_leaf() {
+        let leaf_root_hex = "395aa064aa4c29f7010acfe3f25db9485bbd4b91897b6ad7ad547639252b4d56";
+        let leaf_string = "L123456";
+
+        let leaf_root = &hex::decode(leaf_root_hex).unwrap();
+        let leaf_tree: &[&[u8]] = &[ leaf_string.as_bytes() ];
+        let root = simple_hash_from_byte_slices(leaf_tree);
+        assert_eq!(leaf_root, &root);
+    }
+
+    #[test]
+    fn test_RFC6962_node() {
+        let node_hash_hex = "aa217fe888e47007fa15edab33c2b492a722cb106c64667fc2b044444de66bbb";
+        let left_string = "N123";
+        let right_string = "N456";
+
+        let node_hash = &hex::decode(node_hash_hex).unwrap();
+        let hash = inner_hash(left_string.as_bytes(), right_string.as_bytes());
+        assert_eq!(node_hash, &hash);
+    }
 }
