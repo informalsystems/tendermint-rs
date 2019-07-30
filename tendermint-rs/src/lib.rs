73e29235cc3c36e332e63c4d0545cbb8a4cfaaec
Tony Arcieri <tony@iqlusion.io>
2019-07-30 12:49:28 -0700

    tendermint-rs v0.10.0

--- tendermint-rs/src/lib.rs
+++ tendermint-rs/src/lib.rs
@@ -15,7 +15,7 @@
 #![forbid(unsafe_code)]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tendermint/kms/master/img/tendermint.png",
-    html_root_url = "https://docs.rs/tendermint/0.10.0-rc1"
+    html_root_url = "https://docs.rs/tendermint/0.10.0"
 )]
 
 #[cfg(feature = "amino-types")]
