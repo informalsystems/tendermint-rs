cf7e70234f30d9a3a555fdc9d47e6d3e697f3480
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2019-11-20 00:07:11 +0100

    simple_hash_from_byte_slices takes a Vec instead of slice (#59)
    * simple_hash_from_byte_slices takes a Vec instead of slice
    
    * use Vec<Vec<u8>> with inner recursion method
    
    * simplify tests:
     direct vector initialization instead of going through &[&[u8]]
    
    * Rename simple_hash_from_byte_slices to simple_hash_from_byte_vectors
    
    The method that computes the root from arbitrary given bytes doesn't
    take slices but Rust's array-type: vector. Also, update comment.

--- tendermint/src/validator.rs
+++ tendermint/src/validator.rs
@@ -21,18 +21,12 @@ impl Set {
 
     /// Compute the Merkle root of the validator set
     pub fn hash(self) -> merkle::Hash {
-        // We need to get from Vec<Info> to &[&[u8]] so we can call simple_hash_from_byte_slices.
-        // This looks like: Vec<Info> -> Vec<Vec<u8>> -> Vec<&[u8]> -> &[&[u8]]
-        // Can we simplify this?
-        // Perhaps simple_hash_from_byteslices should take Vec<Vec<u8>> directly ?
         let validator_bytes: Vec<Vec<u8>> = self
             .validators
             .into_iter()
             .map(|x| x.hash_bytes())
             .collect();
-        let validator_byteslices: Vec<&[u8]> =
-            (&validator_bytes).iter().map(|x| x.as_slice()).collect();
-        merkle::simple_hash_from_byte_slices(validator_byteslices.as_slice())
+        merkle::simple_hash_from_byte_vectors(validator_bytes)
     }
 }
 
