7477c9d872472a94d8b1bca0c4ece0150eb5ad53
Alexander Simmerl <a.simmerl@gmail.com>
2019-12-30 22:14:24 +0100

    Shorten height field assignment

--- tendermint/src/rpc/endpoint/commit.rs
+++ tendermint/src/rpc/endpoint/commit.rs
@@ -12,11 +12,12 @@ pub struct Request {
 impl Request {
     /// Create a new request for commit info about a particular block
     pub fn new(height: block::Height) -> Self {
-        let mut height_opt = Some(height);
-        if height.value() == 0 {
-            height_opt = None
+        Self {
+            height: match height.value() {
+                0 => None,
+                _ => Some(height),
+            },
         }
-        Self { height: height_opt }
     }
 }
 
