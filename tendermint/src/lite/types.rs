1671be1d86083f0dfeb0ca612096f762c563ae9b
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2020-03-13 18:05:56 +0100

    fmt

--- tendermint/src/lite/types.rs
+++ tendermint/src/lite/types.rs
@@ -4,9 +4,9 @@
 use std::fmt::Debug;
 use std::time::SystemTime;
 
+use crate::serializers;
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
-use crate::serializers;
 
 use crate::lite::error::{Error, Kind};
 use crate::Hash;
