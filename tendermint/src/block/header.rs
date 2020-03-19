44ce4f97329d27b7c3b3ae55c02f43e7a227ed85
Romain Ruetschi <romain@informal.systems>
2020-03-19 15:52:02 +0100

    Add matching serializer for `app_hash` field of `block::Header` struct
    Closes #187

--- tendermint/src/block/header.rs
+++ tendermint/src/block/header.rs
@@ -59,7 +59,10 @@ pub struct Header {
     pub consensus_hash: Hash,
 
     /// State after txs from the previous block
-    #[serde(deserialize_with = "serializers::parse_hex")]
+    #[serde(
+        serialize_with = "serializers::serialize_hex",
+        deserialize_with = "serializers::parse_hex"
+    )]
     pub app_hash: Vec<u8>,
 
     /// Root hash of all results from the txs from the previous block
