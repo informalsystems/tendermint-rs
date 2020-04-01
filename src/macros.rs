ad92d36d65ea7a8caf10f3e6833ed29afb218e72
Romain Ruetschi <romain@informal.systems>
2020-04-01 13:44:19 +0200

    Add tests

--- src/macros.rs
+++ src/macros.rs
@@ -2,14 +2,14 @@
 macro_rules! predicate {
     ( $name:ident ( $( $field:ident : $typ:ty ),* ) @ $tag:ident $eval:block # $($fmt:expr,)* ) => {
         paste::item! {
-            pub type [<Pred $name>] = $crate::TaggedPredicate<$tag>;
+            pub type [<$name Pred>] = $crate::TaggedPredicate<$tag>;
 
             pub struct $name {
                 $( $field: $typ, )*
             }
 
             impl $name {
-                pub fn pred($( $field : $typ, )*) -> [<Pred $name>] {
+                pub fn pred($( $field : $typ, )*) -> [<$name Pred>] {
                     let p = Self { $( $field, )* };
                     $crate::PredicateExt::tag(p)
                 }
@@ -31,12 +31,38 @@ macro_rules! predicate {
 #[cfg(test)]
 #[allow(dead_code)]
 mod tests {
+    use crate::Predicate;
+
     pub struct Tag;
 
     predicate! {
-        Test(a: u32, b: bool) @ Tag {
+        Test(a: i32, b: bool) @ Tag {
             self.a > 0 && self.b
         } # "{} > 0 && {}", self.a, self.b,
+    }
+
+    #[test]
+    fn macro_pred_works_true() {
+        let p = Test::pred(4, true);
+        assert_eq!(p.eval(), true);
+    }
+
+    #[test]
+    fn macro_pred_works_false1() {
+        let p = Test::pred(-4, true);
+        assert_eq!(p.eval(), false);
+    }
+
+    #[test]
+    fn macro_pred_works_false2() {
+        let p = Test::pred(4, false);
+        assert_eq!(p.eval(), false);
+    }
+
+    #[test]
+    fn macro_pred_format_works() {
+        let p = Test::pred(4, true);
+        assert_eq!(p.to_string(), "4 > 0 && true");
     }
 }
 
@@ -46,7 +72,7 @@ mod tests {
 //     } # "{} > 0 && {}", self.a, self.b
 // }
 //
-// pub type PredTest = TaggedPredicate<Tag>;
+// pub type TestPred = TaggedPredicate<Tag>;
 //
 // #[derive(Debug)]
 // pub struct Test {
@@ -55,7 +81,7 @@ mod tests {
 // }
 //
 // impl Test {
-//     pub fn pred(a: u32, b: bool) -> PredTest {
+//     pub fn pred(a: u32, b: bool) -> TestPred {
 //          let p = Self { a, b );
 //          crate::PredicateExt::tag(p)
 //     }
