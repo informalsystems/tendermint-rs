b97121192c592b143ea34cfc2a3530ad33b7a839
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2020-04-08 12:34:04 +0200

    manually update Proof and introduce ProofOp to match the actual JSON encoding

--- tendermint/src/abci/proof.rs
+++ tendermint/src/abci/proof.rs
@@ -1,51 +1,23 @@
 //! ABCI Merkle proofs
 
-use crate::error::Error;
-use serde::{de::Error as _, Deserialize, Deserializer, Serialize, Serializer};
-use std::{
-    fmt::{self, Display},
-    str::FromStr,
-};
-use subtle_encoding::{Encoding, Hex};
+use serde::{Deserialize, Serialize};
 
-/// ABCI Merkle proofs
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub struct Proof(Vec<u8>);
-
-impl AsRef<[u8]> for Proof {
-    fn as_ref(&self) -> &[u8] {
-        self.0.as_ref()
-    }
+/// Proof is Merkle proof defined by the list of ProofOps
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
+pub struct Proof {
+    /// The list of ProofOps
+    pub ops: Vec<ProofOp>,
 }
 
-impl Display for Proof {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            &Hex::upper_case().encode_to_string(&self.0).unwrap()
-        )
-    }
-}
-
-impl FromStr for Proof {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self, Error> {
-        let bytes = Hex::upper_case().decode(s)?;
-        Ok(Proof(bytes))
-    }
-}
-
-impl<'de> Deserialize<'de> for Proof {
-    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let hex = String::deserialize(deserializer)?;
-        Ok(Self::from_str(&hex).map_err(|e| D::Error::custom(format!("{}", e)))?)
-    }
-}
-
-impl Serialize for Proof {
-    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        self.to_string().serialize(serializer)
-    }
+/// ProofOp defines an operation used for calculating Merkle root
+/// The data could be arbitrary format, providing necessary data
+/// for example neighbouring node hash
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
+pub struct ProofOp {
+    /// Type of the ProofOp
+    pub field_type: String,
+    /// Key of the ProofOp
+    pub key: Vec<u8>,
+    /// Actual data
+    pub data: Vec<u8>,
 }
