3c4f23fce5c893282e6c0bf688f9058839b20e20
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2020-04-11 04:11:18 +0200

    Actually much better: just use enums

--- tendermint/src/amino_types/registered_json.rs
+++ tendermint/src/amino_types/registered_json.rs
@@ -27,12 +27,20 @@ mod test {
         pub value: T,
     }
 
+    #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
+    #[serde(tag = "type", content = "value")]
+    enum SomeRegisteredInterface {
+        #[serde(alias = "cosmos-sdk/MsgUnjail")]
+        Unjail(MsgUnjail),
+        #[serde(alias = "Foo")]
+        OtherMadeUp(MsgOtherMadeUp),
+    }
+
     #[test]
     fn test_simple_example() {
         let json_data =
             r#"{"type":"cosmos-sdk/MsgUnjail","value":{"address":"cosmosvaloper1v93xxeqhg9nn6"}}"#;
         let res = serde_json::from_str::<AminoJSON<MsgUnjail>>(json_data);
-        println!("res: {:?}", res);
         assert!(res.is_ok());
         let msg_unjail = res.unwrap().value;
         println!("{:?}", msg_unjail);
@@ -42,5 +50,25 @@ mod test {
         let res2 = serde_json::from_str::<AminoJSON<MsgOtherMadeUp>>(json_data2);
         println!("res: {:?}", res2);
         test_serialization_roundtrip::<AminoJSON<MsgOtherMadeUp>>(&json_data2);
+    }
+
+    #[test]
+    fn test_enum() {
+        let json_data =
+            r#"{"type":"cosmos-sdk/MsgUnjail","value":{"address":"cosmosvaloper1v93xxeqhg9nn6"}}"#;
+        let res = serde_json::from_str::<SomeRegisteredInterface>(json_data).unwrap();
+        match res {
+            SomeRegisteredInterface::Unjail(m) => println!("{:?}", m),
+            SomeRegisteredInterface::OtherMadeUp(m) => println!("{:?}", m),
+        }
+        test_serialization_roundtrip::<SomeRegisteredInterface>(&json_data);
+
+        let json_data2 = r#"{"type":"Foo","value":{"test":"Bar"}}"#;
+        let res2 = serde_json::from_str::<SomeRegisteredInterface>(json_data2).unwrap();
+        match res2 {
+            SomeRegisteredInterface::Unjail(m) => println!("{:?}", m),
+            SomeRegisteredInterface::OtherMadeUp(m) => println!("{:?}", m),
+        }
+        test_serialization_roundtrip::<SomeRegisteredInterface>(&json_data2);
     }
 }
