4a78872a36bbade050854047d17618ffd91bdc88
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2020-04-17 18:35:36 +0200

    Bump version

--- tendermint/src/lib.rs
+++ tendermint/src/lib.rs
@@ -15,7 +15,7 @@
 #![forbid(unsafe_code)]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tendermint/kms/master/img/tendermint.png",
-    html_root_url = "https://docs.rs/tendermint/0.12.0-rc0"
+    html_root_url = "https://docs.rs/tendermint/0.12.0"
 )]
 
 #[macro_use]
