89ed1380dbc2dc1c800fd43fcd8873d5176fff00
Zaki Manian <zaki@manian.org>
2020-04-17 21:32:40 -0700

    Missed adding file

---
+++ tendermint/src/rpc/subscribe.rs
@@ -0,0 +1,33 @@
+use async_tungstenite::{tokio::connect_async, tokio::TokioAdapter, tungstenite::Message};
+use futures::prelude::*;
+use tokio::net::TcpStream;
+
+pub struct WebSocketEvents {
+    socket: async_tungstenite::WebSocketStream<TokioAdapter<TcpStream>>,
+}
+
+pub enum Event {
+    GenericEvent { data: serde_json::value::Value },
+}
+
+impl WebSocketEvents {
+    pub async fn subscribe(url: &str, query: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let (mut ws_stream, _) = connect_async(&format!("http://{}/subscribe", url)).await?;
+
+        ws_stream.send(Message::text(query)).await?;
+
+        Ok(WebSocketEvents { socket: ws_stream })
+    }
+
+    pub async fn next_event(&mut self) -> Result<Event, Box<dyn std::error::Error>> {
+        let msg = self
+            .socket
+            .next()
+            .await
+            .ok_or_else(|| "web socket closed")??;
+
+        Ok(Event::GenericEvent {
+            data: msg.to_string().parse()?,
+        })
+    }
+}
