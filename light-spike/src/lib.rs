79b26c67482a805f5ef05d2fd1afc036bf0f6d28
Romain Ruetschi <romain@informal.systems>
2020-04-28 12:55:13 +0200

    Silence a couple Clippy warnings

--- light-spike/src/lib.rs
+++ light-spike/src/lib.rs
@@ -1,10 +1,10 @@
-#![deny(rust_2018_idioms)]
-#![deny(nonstandard_style)]
+#![deny(rust_2018_idioms, nonstandard_style)]
 #![warn(
      unreachable_pub,
      // missing_docs,
      // missing_doc_code_examples
  )]
+#![allow(clippy::too_many_arguments, clippy::match_wild_err_arm)]
 
 pub mod components;
 pub mod errors;
