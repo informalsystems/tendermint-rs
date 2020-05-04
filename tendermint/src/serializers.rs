b7005cfde6081d836f0d9f75761f95d2ebe3b003
Greg Szabo <greg@philosobear.com>
2020-05-04 09:48:07 -0400

    Separated CommitSig serialization and validation using serde 'from' annotation.

--- tendermint/src/serializers.rs
+++ tendermint/src/serializers.rs
@@ -2,7 +2,11 @@
 //!
 //! Serializers and deserializers for a transparent developer experience.
 //!
-//! All serializers are presented in a serializers::<Rust_nickname>::<JSON_representation_name> format.
+//! Serializers for the 'with' annotation are presented in a
+//! serializers::<rust_nickname>::<json_representation_name> format.
+//!
+//! Serializers for the 'try_from' annotation are presented in a
+//! serializers::Raw<CustomType> format.
 //!
 //! This example shows how to serialize Vec<u8> into different types of strings:
 //! ```
@@ -24,23 +28,34 @@
 //! }
 //! ```
 //!
-//! Available serializers:
-//! i64                  <-> string:               #[serde(with="serializers::primitives::string")]
-//! u64                  <-> string:               #[serde(with="serializers::primitives::string")]
+//! Available serializer modules for the 'with' annotation:
+//! i64                  <-> string                #[serde(with="serializers::primitives::string")]
+//! u64                  <-> string                #[serde(with="serializers::primitives::string")]
 //! std::time::Duration  <-> nanoseconds as string #[serde(with="serializers::timeduration::string")]
-//! Vec<u8>              <-> HexString:            #[serde(with="serializers::bytes::hexstring")]
-//! Vec<u8>              <-> Base64String:         #[serde(with="serializers::bytes::base64string")]
-//! Vec<u8>              <-> String:               #[serde(with="serializers::bytes::string")]
+//! Vec<u8>              <-> HexString             #[serde(with="serializers::bytes::hexstring")]
+//! Vec<u8>              <-> Base64String          #[serde(with="serializers::bytes::base64string")]
+//! Vec<u8>              <-> string                #[serde(with="serializers::bytes::string")]
 //!
 //! Notes:
 //! * Any type that has the "FromStr" trait can be serialized into a string with serializers::primitives::string.
 //! * serializers::bytes::* deserializes a null value into an empty vec![].
+//!
+//! Available serializer types for the 'try_from' annotation:
+//! block::CommitSig     <-> RawCommitSig          #[serde(try_from="serializers::RawCommitSig")]
+//!
+//! Notes:
+//! * The Raw... types are implemented based on the Tendermint specifications
+//!
 
 pub mod bytes;
 pub mod primitives;
 pub mod timeduration;
 
+mod raw_commit_sig;
+pub use raw_commit_sig::BlockIDFlag;
+pub use raw_commit_sig::RawCommitSig;
+
 /// Todo: refactor and remove
 mod other;
-pub use other::parse_non_empty_hash;
 pub use other::parse_non_empty_block_id;
+pub use other::parse_non_empty_hash;
---
+++ tendermint/src/serializers/raw_commit_sig.rs
@@ -0,0 +1,40 @@
+//! RawCommitSig type for deserialization
+use crate::{account, Signature, Time};
+use serde::{Deserialize, Deserializer};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+// Implements decision: https://github.com/tendermint/tendermint/blob/master/docs/architecture/adr-025-commit.md#decision
+
+/// indicate which BlockID the signature is for
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug)]
+#[repr(u8)]
+pub enum BlockIDFlag {
+    /// vote is not included in the Commit.Precommits
+    BlockIDFlagAbsent = 1,
+    /// voted for the Commit.BlockID
+    BlockIDFlagCommit = 2,
+    /// voted for nil
+    BlockIDFlagNil = 3,
+}
+
+/// RawCommitSig struct for interim deserialization of JSON object
+#[derive(Deserialize)]
+pub struct RawCommitSig {
+    /// indicate which BlockID the signature is for
+    pub block_id_flag: BlockIDFlag,
+    /// Validator Address
+    pub validator_address: account::Id,
+    /// Timestamp
+    #[serde(default)]
+    pub timestamp: Option<Time>,
+    /// Signature
+    #[serde(default, deserialize_with = "option_signature")]
+    pub signature: Option<Signature>,
+}
+
+fn option_signature<'de, D>(deserializer: D) -> Result<Option<Signature>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    Deserialize::deserialize(deserializer).map(|x: Option<_>| x.unwrap_or(None))
+}
