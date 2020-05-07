772a16f3e7ed6189537fc9ca93a02dbdd5246bca
Romain Ruetschi <romain@informal.systems>
2020-05-07 15:42:19 +0200

    Update example to prod implementations

--- light-spike/examples/light_client.rs
+++ light-spike/examples/light_client.rs
@@ -1,7 +1,5 @@
-use anomaly::BoxError;
-
 use light_spike::components::scheduler;
-use light_spike::predicates::production::ProductionPredicates;
+use light_spike::predicates::production::ProdPredicates;
 use light_spike::prelude::*;
 
 use std::collections::HashMap;
@@ -15,7 +13,7 @@ pub fn main() {
     let primary: Peer = "tcp://127.0.0.1:26657".parse().unwrap();
     let mut io = RealIo::new();
 
-    let trusted_state = io.fetch_light_block(primary.clone(), 1520).unwrap();
+    let trusted_state = io.fetch_light_block(primary.clone(), 9977).unwrap();
 
     trusted_store_writer.add(trusted_state);
 
@@ -42,10 +40,10 @@ pub fn main() {
         now: Time::now(),
     };
 
-    let predicates = MockPredicates;
-    let voting_power_calculator = MockVotingPower;
-    let commit_validator = MockCommitValidator;
-    let header_hasher = MockHeaderHasher;
+    let predicates = ProdPredicates;
+    let voting_power_calculator = ProdVotingPowerCalculator;
+    let commit_validator = ProdCommitValidator;
+    let header_hasher = ProdHeaderHasher;
 
     let verifier = RealVerifier::new(
         predicates,
@@ -71,170 +69,3 @@ pub fn main() {
     demuxer.run().unwrap();
 }
 
-#[derive(Copy, Clone)]
-struct MockHeaderHasher;
-impl HeaderHasher for MockHeaderHasher {
-    fn hash(&self, header: &Header) -> Hash {
-        header.consensus_hash // FIXME: wrong hash
-    }
-}
-
-#[derive(Copy, Clone)]
-struct MockCommitValidator;
-impl CommitValidator for MockCommitValidator {
-    fn validate(
-        &self,
-        _commit: &Commit,
-        _validators: &ValidatorSet,
-    ) -> Result<(), anomaly::BoxError> {
-        // let first_byte = commit.header_hash.as_bytes()[0];
-        // if first_byte < 90 {
-        //     return Err("invalid commit".into());
-        // }
-
-        Ok(())
-    }
-}
-
-#[derive(Copy, Clone)]
-struct MockVotingPower;
-impl VotingPowerCalculator for MockVotingPower {
-    fn total_power_of(&self, _validators: &ValidatorSet) -> u64 {
-        8
-    }
-    fn voting_power_in(
-        &self,
-        _sh: &SignedHeader,
-        _validators: &ValidatorSet,
-    ) -> Result<u64, BoxError> {
-        Ok(4)
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
-pub struct MockPredicates;
-
-impl VerificationPredicates for MockPredicates {
-    fn validator_sets_match(
-        &self,
-        signed_header: &SignedHeader,
-        validators: &ValidatorSet,
-    ) -> Result<(), VerificationError> {
-        ProductionPredicates.validator_sets_match(signed_header, validators)
-    }
-
-    fn next_validators_match(
-        &self,
-        signed_header: &SignedHeader,
-        validators: &ValidatorSet,
-    ) -> Result<(), VerificationError> {
-        ProductionPredicates.next_validators_match(signed_header, validators)
-    }
-
-    fn header_matches_commit(
-        &self,
-        header: &Header,
-        signed_header: &SignedHeader,
-        header_hasher: &dyn HeaderHasher,
-    ) -> Result<(), VerificationError> {
-        ProductionPredicates.header_matches_commit(header, signed_header, header_hasher)
-    }
-
-    fn valid_commit(
-        &self,
-        commit: &Commit,
-        validators: &ValidatorSet,
-        validator: &dyn CommitValidator,
-    ) -> Result<(), VerificationError> {
-        ProductionPredicates.valid_commit(commit, validators, validator)
-    }
-
-    fn is_within_trust_period(
-        &self,
-        header: &Header,
-        trusting_period: Duration,
-        now: Time,
-    ) -> Result<(), VerificationError> {
-        ProductionPredicates.is_within_trust_period(header, trusting_period, now)
-    }
-
-    fn is_monotonic_bft_time(
-        &self,
-        untrusted_header: &Header,
-        trusted_header: &Header,
-    ) -> Result<(), VerificationError> {
-        ProductionPredicates.is_monotonic_bft_time(untrusted_header, trusted_header)
-    }
-
-    fn is_monotonic_height(
-        &self,
-        untrusted_header: &Header,
-        trusted_header: &Header,
-    ) -> Result<(), VerificationError> {
-        ProductionPredicates.is_monotonic_height(untrusted_header, trusted_header)
-    }
-
-    fn has_sufficient_voting_power(
-        &self,
-        signed_header: &SignedHeader,
-        validators: &ValidatorSet,
-        trust_threshold: &TrustThreshold,
-        calculator: &dyn VotingPowerCalculator,
-    ) -> Result<(), VerificationError> {
-        use tendermint::lite::types::Commit;
-
-        let first_byte = signed_header.header_hash().as_bytes()[0];
-
-        if first_byte > 140 {
-            return Err(VerificationError::InsufficientVotingPower {
-                total_power: 0,
-                voting_power: Some(0),
-            });
-        }
-
-        ProductionPredicates.has_sufficient_voting_power(
-            signed_header,
-            validators,
-            trust_threshold,
-            calculator,
-        )
-    }
-
-    fn has_sufficient_validators_overlap(
-        &self,
-        untrusted_sh: &SignedHeader,
-        trusted_validators: &ValidatorSet,
-        trust_threshold: &TrustThreshold,
-        calculator: &dyn VotingPowerCalculator,
-    ) -> Result<(), VerificationError> {
-        ProductionPredicates.has_sufficient_validators_overlap(
-            untrusted_sh,
-            trusted_validators,
-            trust_threshold,
-            calculator,
-        )
-    }
-
-    fn has_sufficient_signers_overlap(
-        &self,
-        untrusted_sh: &SignedHeader,
-        untrusted_validators: &ValidatorSet,
-        trust_threshold: &TrustThreshold,
-        calculator: &dyn VotingPowerCalculator,
-    ) -> Result<(), VerificationError> {
-        ProductionPredicates.has_sufficient_signers_overlap(
-            untrusted_sh,
-            untrusted_validators,
-            trust_threshold,
-            calculator,
-        )
-    }
-
-    fn valid_next_validator_set(
-        &self,
-        untrusted_sh: &SignedHeader,
-        untrusted_next_vals: &ValidatorSet,
-    ) -> Result<(), VerificationError> {
-        ProductionPredicates.valid_next_validator_set(untrusted_sh, untrusted_next_vals)
-    }
-}
