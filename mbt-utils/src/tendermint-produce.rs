19d0a3cef887642b7d8d45c337670c31a7e8ea99
Andrey Kuprianov <andrey@informal.systems>
2020-06-19 16:52:12 +0200

    mbt-utils/produce header (not complete yet)

--- mbt-utils/src/tendermint-produce.rs
+++ mbt-utils/src/tendermint-produce.rs
@@ -8,6 +8,11 @@ use tendermint::validator::{Info, ProposerPriority};
 use tendermint::vote::Power;
 use tendermint::public_key::PublicKey;
 use std::process::exit;
+use tendermint::block::*;
+use tendermint::block::header::Version;
+use tendermint::{Time, validator, chain};
+use tendermint::lite::ValidatorSet;
+use std::str::FromStr;
 
 
 #[derive(Debug, Options)]
@@ -23,7 +28,12 @@ struct CliOptions {
 enum Command {
     #[options(help = "produce validator from an identifier, passed via STDIN")]
     Validator(ValidatorOpts),
+    #[options(help = "produce header, from an array of validators passed via STDIN")]
+    Header(HeaderOpts),
+
 }
+
+
 
 fn main() {
     let opts = CliOptions::parse_args_default_or_exit();
@@ -36,6 +46,7 @@ fn main() {
             std::process::exit(1);
         }
         Some(Command::Validator(opts)) => produce_validator(opts),
+        Some(Command::Header(opts)) => produce_header(opts),
     }
 }
 
@@ -70,4 +81,39 @@ fn produce_validator(opts: ValidatorOpts) {
         info.proposer_priority = Some(ProposerPriority::new(priority));
     }
     println!("{}", serde_json::to_string(&info).unwrap());
+}
+
+#[derive(Debug, Options)]
+struct HeaderOpts {
+    #[options(help = "print this help and exit")]
+    help: bool,
+}
+
+fn produce_header(_opts: HeaderOpts) {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    if let Ok(vals) = serde_json::from_str::<Vec<Info>>(buffer.as_str()) {
+        if vals.is_empty() {
+            eprintln!("can't produce a header for empty validator set");
+            exit(1)
+        }
+        let valset = validator::Set::new(vals.clone());
+        let header = Header {
+            version: Version { block: 0, app: 0 },
+            chain_id: chain::Id::from_str("test-chain-01").unwrap(),
+            height: Default::default(),
+            time: Time::now(),
+            last_block_id: None,
+            last_commit_hash: None,
+            data_hash: None,
+            validators_hash: valset.hash(),
+            next_validators_hash: valset.hash(),
+            consensus_hash: valset.hash(),
+            app_hash: vec![],
+            last_results_hash: None,
+            evidence_hash: None,
+            proposer_address: vals[0].address.clone()
+        };
+        println!("{}", serde_json::to_string(&header).unwrap());
+    }
 }
