a14d1a801c4d7432eec7c5f0511253dad41701ef
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2020-07-09 23:28:00 +0200

    make rpc server addr overridable via cli argument

--- light-node/src/commands/start.rs
+++ light-node/src/commands/start.rs
@@ -9,11 +9,20 @@ use crate::application::APPLICATION;
 use crate::config::LightNodeConfig;
 
 use abscissa_core::path::PathBuf;
+use std::net::SocketAddr;
 
 /// `start` subcommand
 ///
 #[derive(Command, Debug, Options)]
 pub struct StartCmd {
+    /// Path to configuration file
+    #[options(
+        short = "l",
+        long = "listen",
+        help = "address the rpc server will serve"
+    )]
+    pub listen_addr: Option<SocketAddr>,
+
     /// Path to configuration file
     #[options(short = "c", long = "config", help = "path to light_node.toml")]
     pub config: Option<PathBuf>,
@@ -36,7 +45,14 @@ impl config::Override<LightNodeConfig> for StartCmd {
     // Process the given command line options, overriding settings from
     // a configuration file using explicit flags taken from command-line
     // arguments.
-    fn override_config(&self, config: LightNodeConfig) -> Result<LightNodeConfig, FrameworkError> {
+    fn override_config(
+        &self,
+        mut config: LightNodeConfig,
+    ) -> Result<LightNodeConfig, FrameworkError> {
+        // Todo figure out if other options would be reasonable to overwrite via CLI arguments.
+        if let Some(addr) = self.listen_addr {
+            config.rpc_config.listen_addr = addr;
+        }
         Ok(config)
     }
 }
