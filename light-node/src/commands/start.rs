d4f34899834333537433eef3640a9549731d6602
Ismail Khoffi <Ismail.Khoffi@gmail.com>
2020-07-16 11:10:21 +0200

    Add info output and fix main output  - from `synced to block {} 1234` to `synced to block: 1234`

--- light-node/src/commands/start.rs
+++ light-node/src/commands/start.rs
@@ -69,7 +69,7 @@ impl Runnable for StartCmd {
             loop {
                 match handle.verify_to_highest() {
                     Ok(light_block) => {
-                        status_info!("synced to block {}", light_block.height().to_string());
+                        status_info!("synced to block:", light_block.height().to_string());
                     }
                     Err(err) => {
                         status_err!("sync failed: {}", err);
@@ -156,6 +156,7 @@ impl StartCmd {
         let laddr = app_config().rpc_config.listen_addr;
         // TODO(liamsi): figure out how to handle the potential error on run
         std::thread::spawn(move || rpc::run(server, &laddr.to_string()));
+        status_info!("started RPC server:", laddr.to_string());
     }
 }
 
