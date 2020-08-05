08450a75cf799bdc7359ef5742d4ac82b9a67eae
Andrey Kuprianov <andrey@informal.systems>
2020-08-05 13:41:14 +0200

    #414: more structure for test cases

--- tendermint/tests/lite-model-based.rs
+++ tendermint/tests/lite-model-based.rs
@@ -8,25 +8,35 @@ use tendermint::block::{Header, Height};
 use tendermint::lite::{Requester, TrustThresholdFraction, TrustedState};
 mod lite_tests;
 use lite_tests::*;
+use tendermint::lite::error::Error;
 
 type Trusted = lite::TrustedState<SignedHeader, Header>;
 
-/// A test case is the initial trusted block,
-/// plus a sequence of input blocks, each with the expected result
-#[derive(Deserialize, Clone, Debug)]
-pub struct ApalacheTests {
-    description: String,
-    model: String,
-    length_bound: Option<u64>,
-    timeout: Option<u64>,
-    tests: Vec<String>,
+#[derive(Deserialize, Clone, Debug, PartialEq)]
+pub enum LiteTestKind {
+    SingleStep,
+    Bisection
 }
 
-/// A test case is the initial trusted block,
-/// plus a sequence of input blocks, each with the expected verdict
+/// An abstraction of the LightClient verification verdict
+#[derive(Deserialize, Clone, Debug, PartialEq)]
+pub enum LiteVerdict {
+    /// verified successfully
+    OK,
+    /// outside of trusting period
+    FAILED_TRUSTING_PERIOD,
+    /// block verification based on the header and commit structure failed
+    FAILED_VERIFICATION,
+    /// passed block verification, but the validator set is too different to verify it
+    CANNOT_VERIFY
+}
+
+/// A single-step test case is a test for `lite::verify_single()` function.
+/// It contains an initial trusted block, plus a sequence of input blocks,
+/// each with the expected verdict.
 /// The trusted state is to be updated only if the verdict is "OK"
 #[derive(Deserialize, Clone, Debug)]
-pub struct SingleTestCase {
+pub struct SingleStepTestCase {
     description: String,
     initial: Initial,
     input: Vec<BlockVerdict>,
@@ -36,7 +46,25 @@ pub struct SingleTestCase {
 #[derive(Deserialize, Clone, Debug)]
 pub struct BlockVerdict {
     block: LiteBlock,
-    verdict: String,
+    verdict: LiteVerdict,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct ApalacheTestBatch {
+    description: String,
+    kind: LiteTestKind,
+    model: String,
+    length_bound: Option<u64>,
+    timeout: Option<u64>,
+    tests: Vec<String>,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct ApalacheTestCase {
+    model: String,
+    test: String,
+    length_bound: Option<u64>,
+    timeout: Option<u64>,
 }
 
 const TEST_DIR: &str = "./tests/support/lite-model-based/";
@@ -45,11 +73,11 @@ fn read_file(dir: &str, file: &str) -> String {
     fs::read_to_string(PathBuf::from(dir.to_owned() + file)).unwrap()
 }
 
-fn read_single_test_case(dir: &str, file: &str) -> SingleTestCase {
+fn read_single_step_test(dir: &str, file: &str) -> SingleStepTestCase {
     serde_json::from_str(read_file(dir, file).as_str()).unwrap()
 }
 
-fn run_single_test_case(tc: &SingleTestCase) {
+fn run_single_step_test(tc: &SingleStepTestCase) {
     let trusted_next_vals = tc.initial.clone().next_validator_set;
     let mut latest_trusted =
         Trusted::new(tc.initial.signed_header.clone().into(), trusted_next_vals);
@@ -76,7 +104,7 @@ fn run_single_test_case(tc: &SingleTestCase) {
             now,
         ) {
             Ok(new_state) => {
-                assert_eq!(input.verdict, "OK");
+                assert_eq!(input.verdict, LiteVerdict::OK);
                 let expected_state = TrustedState::new(
                     untrusted_signed_header.clone().into(),
                     untrusted_next_vals.clone(),
@@ -86,14 +114,14 @@ fn run_single_test_case(tc: &SingleTestCase) {
                 test_serialization_roundtrip(&latest_trusted);
             }
             Err(e) => {
-                assert_ne!(input.verdict, "OK");
+                assert_ne!(input.verdict, LiteVerdict::OK);
             }
         }
     }
 }
 
 #[test]
-fn apalache() {
-    let tc = read_single_test_case(TEST_DIR, "first-model-based-test.json");
-    run_test_case(&tc);
+fn single_step_test() {
+    let tc = read_single_step_test(TEST_DIR, "first-model-based-test.json");
+    run_single_step_test(&tc);
 }
