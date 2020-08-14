f01d13d22f2393c7841c21f054e0c43f7aab6b5b
Thane Thomson <connect@thanethomson.com>
2020-08-14 12:52:35 -0400

    Expose client docs in base RPC package
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -1,4 +1,30 @@
 //! Tendermint RPC definitions and types.
+//!
+//! ## Client
+//!
+//! Available when specifying the `client` feature flag.
+//!
+//! The RPC client comes in two flavors: a [`MinimalClient`] and a
+//! [`FullClient`]. A `MinimalClient` implementation provides access to all
+//! RPC endpoints with the exception of the [`Event`] subscription ones,
+//! whereas a `FullClient` implementation provides access to all RPC
+//! functionality. The reason for this distinction is because `Event`
+//! subscription usually requires more resources to manage, and may not be
+//! necessary for all applications making use of the Tendermint RPC.
+//!
+//! Transport-specific client support is provided by way of additional feature
+//! flags (where right now we only have one transport, but intend on providing
+//! more in future):
+//!
+//! * `http_ws`: Provides an HTTP interface for request/response interactions
+//!   (see [`HttpClient`]), and a WebSocket-based interface for `Event`
+//!   subscription (see [`HttpWebSocketClient`]).
+//!
+//! [`MinimalClient`]: trait.MinimalClient.html
+//! [`FullClient`]: trait.FullClient.html
+//! [`Event`]: event/struct.Event.html
+//! [`HttpClient`]: struct.HttpClient.html
+//! [`HttpWebSocketClient`]: struct.HttpWebSocketClient.html
 
 #[cfg(feature = "client")]
 mod client;
