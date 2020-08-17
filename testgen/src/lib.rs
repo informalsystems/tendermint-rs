45d672e0ff2f136ef643c42079b07ff75d33cc5b
Andrey Kuprianov <andrey@informal.systems>
2020-08-17 18:50:55 +0200

    #524 cargo fmt

--- testgen/src/lib.rs
+++ testgen/src/lib.rs
@@ -5,14 +5,14 @@ pub mod commit;
 pub mod consensus;
 pub mod generator;
 pub mod header;
+pub mod tester;
 pub mod validator;
 pub mod vote;
-pub mod tester;
 
 pub use commit::Commit;
 pub use generator::Generator;
 pub use header::Header;
+pub use tester::TestEnv;
+pub use tester::Tester;
 pub use validator::Validator;
 pub use vote::Vote;
-pub use tester::Tester;
-pub use tester::TestEnv;
--- testgen/src/tester.rs
+++ testgen/src/tester.rs
@@ -1,17 +1,17 @@
-use crate::{helpers::*};
+use crate::helpers::*;
+use crate::tester::TestResult::{Failure, ParseError, ReadError, Success};
+use serde::de::DeserializeOwned;
+use std::panic::UnwindSafe;
+use std::{fs, path::PathBuf};
 use std::{
     panic,
     sync::{Arc, Mutex},
 };
-use serde::{de::DeserializeOwned};
-use std::{fs, path::PathBuf};
-use std::panic::{UnwindSafe};
-use crate::tester::TestResult::{Success, Failure, ParseError, ReadError};
 
 #[derive(Debug, Clone)]
 pub struct TestEnv {
     root_dir: String,
-    logs: Vec<String>
+    logs: Vec<String>,
 }
 
 impl TestEnv {
@@ -25,8 +25,8 @@ impl TestEnv {
             None => None,
             Some(full_path) => match fs::read_to_string(&full_path) {
                 Ok(file) => Some(file),
-                Err(_) => None
-            }
+                Err(_) => None,
+            },
         }
     }
 
@@ -36,7 +36,7 @@ impl TestEnv {
         let full_path = PathBuf::from(&self.root_dir).join(rel_path);
         match full_path.to_str() {
             None => None,
-            Some(full_path) => Some(full_path.to_string())
+            Some(full_path) => Some(full_path.to_string()),
         }
     }
 
@@ -47,8 +47,8 @@ impl TestEnv {
             Err(_) => None,
             Ok(rel_path) => match rel_path.to_str() {
                 None => None,
-                Some(rel_path) => Some(rel_path.to_string())
-            }
+                Some(rel_path) => Some(rel_path.to_string()),
+            },
         }
     }
 }
@@ -56,7 +56,7 @@ impl TestEnv {
 pub struct Tester {
     root_dir: String,
     tests: Vec<(String, Box<dyn Fn(&str) -> TestResult>)>,
-    results: std::collections::BTreeMap<String, Vec<(String, TestResult)>>
+    results: std::collections::BTreeMap<String, Vec<(String, TestResult)>>,
 }
 
 #[derive(Debug, Clone)]
@@ -64,24 +64,36 @@ pub enum TestResult {
     ReadError,
     ParseError,
     Success(TestEnv),
-    Failure {
-        message: String,
-        location: String
-    }
+    Failure { message: String, location: String },
 }
 
 impl TestResult {
     pub fn is_success(&self) -> bool {
-        match self { TestResult::Success(_) => true, _ => false }
+        match self {
+            TestResult::Success(_) => true,
+            _ => false,
+        }
     }
     pub fn is_failure(&self) -> bool {
-        match self { TestResult::Failure{ message: _, location: _ } => true, _ => false }
+        match self {
+            TestResult::Failure {
+                message: _,
+                location: _,
+            } => true,
+            _ => false,
+        }
     }
     pub fn is_readerror(&self) -> bool {
-        match self { TestResult::ReadError => true, _ => false }
+        match self {
+            TestResult::ReadError => true,
+            _ => false,
+        }
     }
     pub fn is_parseerror(&self) -> bool {
-        match self { TestResult::ParseError => true, _ => false }
+        match self {
+            TestResult::ParseError => true,
+            _ => false,
+        }
     }
 }
 
@@ -90,19 +102,20 @@ impl Tester {
         Tester {
             root_dir: root_dir.to_string(),
             tests: vec![],
-            results: Default::default()
+            results: Default::default(),
         }
     }
 
     pub fn env(&self) -> TestEnv {
         TestEnv {
             root_dir: self.root_dir.clone(),
-            logs: vec![]
+            logs: vec![],
         }
     }
 
     fn capture_test<F>(env: TestEnv, test: F) -> TestResult
-    where F: FnOnce(TestEnv) -> TestEnv + UnwindSafe
+    where
+        F: FnOnce(TestEnv) -> TestEnv + UnwindSafe,
     {
         let test_result = Arc::new(Mutex::new(ParseError));
         let old_hook = panic::take_hook();
@@ -115,79 +128,75 @@ impl Tester {
                     None => match info.payload().downcast_ref::<String>() {
                         Some(s) => s.clone(),
                         None => "Unknown error".to_string(),
-                    }
+                    },
                 };
                 let location = match info.location() {
                     Some(l) => l.to_string(),
-                    None => "".to_string()
+                    None => "".to_string(),
                 };
-                *result = Failure {
-                    message,
-                    location
-                };
+                *result = Failure { message, location };
             })
         });
-        let test_fun = || {
-            test(env.clone())
-        };
+        let test_fun = || test(env.clone());
         let result = panic::catch_unwind(test_fun);
         panic::set_hook(old_hook);
         match result {
             Ok(res) => Success(res),
-            Err(_) => (*test_result.lock().unwrap()).clone()
+            Err(_) => (*test_result.lock().unwrap()).clone(),
         }
     }
 
     pub fn add_test<T>(&mut self, name: &str, test: fn(T))
-        where
-            T: 'static + DeserializeOwned + UnwindSafe,
+    where
+        T: 'static + DeserializeOwned + UnwindSafe,
     {
         let test_env = self.env();
-        let test_fn = move |input: &str| {
-            match parse_as::<T>(&input) {
-                Ok(test_case) => Tester::capture_test(test_env.clone(), |env| {
-                    test(test_case);
-                    env
-                }),
-                Err(_) => ParseError
-            }
+        let test_fn = move |input: &str| match parse_as::<T>(&input) {
+            Ok(test_case) => Tester::capture_test(test_env.clone(), |env| {
+                test(test_case);
+                env
+            }),
+            Err(_) => ParseError,
         };
         self.tests.push((name.to_string(), Box::new(test_fn)));
     }
 
     pub fn add_test_with_env<T>(&mut self, name: &str, test: fn(T, &mut TestEnv))
-        where
-            T: 'static + DeserializeOwned + UnwindSafe,
+    where
+        T: 'static + DeserializeOwned + UnwindSafe,
     {
         let test_env = self.env();
-        let test_fn = move |input: &str| {
-            match parse_as::<T>(&input) {
-                Ok(test_case) => Tester::capture_test(test_env.clone(), |env| {
-                    let mut env = env;
-                    test(test_case, &mut env);
-                    env
-                }),
-                Err(_) => ParseError
-            }
+        let test_fn = move |input: &str| match parse_as::<T>(&input) {
+            Ok(test_case) => Tester::capture_test(test_env.clone(), |env| {
+                let mut env = env;
+                test(test_case, &mut env);
+                env
+            }),
+            Err(_) => ParseError,
         };
         self.tests.push((name.to_string(), Box::new(test_fn)));
     }
 
     fn add_result(&mut self, name: &str, path: &str, result: TestResult) {
-        self.results.entry(name.to_string()).or_insert(Vec::new()).
-            push((path.to_string(), result))
+        self.results
+            .entry(name.to_string())
+            .or_insert(Vec::new())
+            .push((path.to_string(), result))
     }
 
     fn read_error(&mut self, path: &str) {
-        self.results.entry("".to_string()).or_insert(Vec::new()).
-            push((path.to_string(), TestResult::ReadError))
+        self.results
+            .entry("".to_string())
+            .or_insert(Vec::new())
+            .push((path.to_string(), TestResult::ReadError))
     }
 
     fn parse_error(&mut self, path: &str) {
-        self.results.entry("".to_string()).or_insert(Vec::new()).
-            push((path.to_string(), TestResult::ParseError))
+        self.results
+            .entry("".to_string())
+            .or_insert(Vec::new())
+            .push((path.to_string(), TestResult::ParseError))
     }
-
 
     pub fn successful_tests(&self, test: &str) -> Vec<(String, TestEnv)> {
         let mut tests = Vec::new();
@@ -275,8 +284,7 @@ impl Tester {
         }
     }
 
-    pub fn run_for_file(&mut self, path: &str)
-    {
+    pub fn run_for_file(&mut self, path: &str) {
         match self.env().read_file(path) {
             None => self.read_error(path),
             Some(input) => {
@@ -284,14 +292,12 @@ impl Tester {
                 for (name, test) in &self.tests {
                     match test(&input) {
                         TestResult::ParseError => continue,
-                        res =>
-                            results.push((name.to_string(), path, res))
+                        res => results.push((name.to_string(), path, res)),
                     }
                 }
                 if results.is_empty() {
                     self.parse_error(path);
-                }
-                else {
+                } else {
                     for (name, path, res) in results {
                         self.add_result(&name, path, res)
                     }
@@ -300,41 +306,32 @@ impl Tester {
         }
     }
 
-    pub fn run_foreach_in_dir(&mut self, dir: &str)
-    {
+    pub fn run_foreach_in_dir(&mut self, dir: &str) {
         let full_dir = PathBuf::from(&self.root_dir).join(dir);
         match full_dir.to_str() {
             None => self.read_error(dir),
-            Some(full_dir) => {
-                match fs::read_dir(full_dir) {
-                    Err(_) => self.read_error(full_dir),
-                    Ok(paths) => {
-                        for path in paths {
-                            if let Ok(entry) = path {
-                                if let Ok(kind) = entry.file_type() {
-                                    let path = format!("{}", entry.path().display());
-                                    let rel_path = self.env().rel_path(&path).unwrap();
-                                    if kind.is_file() || kind.is_symlink() {
-                                        if !rel_path.ends_with(".json") {
-                                            continue
-                                        }
-                                        else {
-                                            self.run_for_file(&rel_path);
-                                        }
+            Some(full_dir) => match fs::read_dir(full_dir) {
+                Err(_) => self.read_error(full_dir),
+                Ok(paths) => {
+                    for path in paths {
+                        if let Ok(entry) = path {
+                            if let Ok(kind) = entry.file_type() {
+                                let path = format!("{}", entry.path().display());
+                                let rel_path = self.env().rel_path(&path).unwrap();
+                                if kind.is_file() || kind.is_symlink() {
+                                    if !rel_path.ends_with(".json") {
+                                        continue;
+                                    } else {
+                                        self.run_for_file(&rel_path);
                                     }
-                                    else if kind.is_dir() {
-                                        self.run_foreach_in_dir(&rel_path);
-                                    }
+                                } else if kind.is_dir() {
+                                    self.run_foreach_in_dir(&rel_path);
                                 }
                             }
                         }
                     }
                 }
-            }
+            },
         }
     }
 }
-
-
-
-
