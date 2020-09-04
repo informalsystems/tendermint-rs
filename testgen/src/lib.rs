9d4531a45603e0f4f86a3b98a7725088ba85fde1
Andrey Kuprianov <andrey@informal.systems>
2020-09-04 11:58:33 +0200

    remove ref to time: it's in another PR

--- testgen/src/lib.rs
+++ testgen/src/lib.rs
@@ -6,14 +6,12 @@ pub mod commit;
 pub mod consensus;
 pub mod generator;
 pub mod header;
-pub mod time;
 pub mod validator;
 pub mod vote;
 
 pub use commit::Commit;
 pub use generator::Generator;
 pub use header::Header;
-pub use time::Time;
 pub use validator::Validator;
 pub use vote::Vote;
 
