61420de4f91a4f5f820fdd4557ad08f2a92ea2bb
Andrey Kuprianov <andrey@informal.systems>
2020-09-04 11:24:46 +0200

    cargo fmt

--- testgen/src/lib.rs
+++ testgen/src/lib.rs
@@ -5,16 +5,16 @@ pub mod commit;
 pub mod consensus;
 pub mod generator;
 pub mod header;
-pub mod time;
 pub mod tester;
+pub mod time;
 pub mod validator;
 pub mod vote;
 
 pub use commit::Commit;
 pub use generator::Generator;
 pub use header::Header;
-pub use time::Time;
 pub use tester::TestEnv;
 pub use tester::Tester;
+pub use time::Time;
 pub use validator::Validator;
 pub use vote::Vote;
