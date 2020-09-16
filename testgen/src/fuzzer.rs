994a8f325ad8e95564e01737ac1e2e1e4e487b7f
Andrey Kuprianov <andrey@informal.systems>
2020-09-16 10:31:24 +0200

    #564 add Fuzzer::is_from(), get_bool(), get_u64(), get_i64(), get_string()

--- testgen/src/fuzzer.rs
+++ testgen/src/fuzzer.rs
@@ -1,9 +1,46 @@
 /// A Fuzzer is anything that can produce an infinite random sequence of numbers.
 /// 0 means no fuzzing, and any other number means fuzzing depending on the number.
 pub trait Fuzzer {
+    /// Get the next random number from the sequence
     fn next(&mut self) -> u64;
+
+    /// Get the current (latest) number from the sequence; also refered to as the current state.
+    fn current(& self) -> u64;
+
+    /// Check if the current number is alternative 'alt' from 'total' number of alternatives.
+    /// It is expected that 0 < alt <= total.
+    /// If the current number is non-zero, then at least one of the alternatives will hold.
+    /// If the current number is zero, none of the alternatives should hold.
+    fn is_from(&self, alt: u64, total: u64) -> bool {
+        self.current() % total == (alt + 1)
+    }
+
+    /// Get the random bool value encoded in the current state
+    fn get_bool(&self) -> bool {
+        self.current() % 2 == 1
+    }
+
+    /// Get the random u64 value encoded in the current state
+    fn get_u64(&self) -> u64 {
+        self.current()
+    }
+
+    /// Get the random i64 value encoded in the current state
+    fn get_i64(&self) -> i64 {
+        let cur = self.current();
+        let max = u64::MAX / 2;
+        if cur >= max {
+             - ((cur % max) as i64)
+        } else {
+            cur as i64
+        }
+    }
+
+    /// Get the random string encoded in the current state
+    fn get_string(&self) -> String;
 }
 
+/// A Fuzzer that doesn't do any fuzzing (always returns 0).
 pub struct NoFuzz {
 }
 
@@ -16,6 +53,12 @@ impl NoFuzz {
 impl Fuzzer for NoFuzz {
     fn next(&mut self) -> u64 {
         0
+    }
+    fn current(&self) -> u64 {
+        0
+    }
+    fn get_string(&self) -> String {
+        String::new()
     }
 }
 
@@ -38,5 +81,13 @@ impl Fuzzer for LogFuzzer {
         let next = self.fuzzer.next();
         self.log.push(next);
         next
+    }
+
+    fn current(&self) -> u64 {
+        self.fuzzer.current()
+    }
+
+    fn get_string(&self) -> String {
+        self.fuzzer.get_string()
     }
 }
