06919ee72f686c4a4820551c58d9f2d03be6f480
Andrey Kuprianov <andrey@informal.systems>
2020-10-08 16:50:28 +0200

    #618 update model-based README.md

--- testgen/src/tester.rs
+++ testgen/src/tester.rs
@@ -402,8 +402,8 @@ impl Tester {
                 match batch(path, input) {
                     None => continue,
                     Some(tests) => {
-                        for (_name, input) in tests {
-                            let test_path = path.to_string(); //  for separate subdirs + "/" + &name
+                        for (name, input) in tests {
+                            let test_path = path.to_string() + "/" + &name;
                             res_tests.push((test_path, input));
                         }
                     }
