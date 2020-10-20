59199be1afe2abac5420ce19a208de11095c158c
Thane Thomson <connect@thanethomson.com>
2020-10-20 11:26:59 -0400

    Add test for JSON timestamp de/serialization precision
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- proto/src/serializers/option_timestamp.rs
+++ proto/src/serializers/option_timestamp.rs
@@ -30,8 +30,27 @@ where
     }
     match Utc.timestamp_opt(value.seconds, value.nanos as u32) {
         LocalResult::None => Err(S::Error::custom("invalid time")),
-        LocalResult::Single(t) => Ok(t.to_rfc3339_opts(SecondsFormat::Millis, true)),
+        LocalResult::Single(t) => Ok(t.to_rfc3339_opts(SecondsFormat::AutoSi, true)),
         LocalResult::Ambiguous(_, _) => Err(S::Error::custom("ambiguous time")),
     }?
     .serialize(serializer)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::google::protobuf::Timestamp;
+    use serde::{Deserialize, Serialize};
+    use serde_json;
+
+    #[test]
+    fn json_timestamp_precision() {
+        #[derive(Serialize, Deserialize)]
+        struct Outer {
+            #[serde(with = "crate::serializers::option_timestamp")]
+            timestamp: Option<Timestamp>,
+        }
+        let json = r#"{"timestamp":"2020-09-14T16:33:54.21191421Z"}"#;
+        let outer = serde_json::from_str::<Outer>(json).unwrap();
+        assert_eq!(json, serde_json::to_string(&outer).unwrap());
+    }
 }
