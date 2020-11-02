687bd125f152c0bd55396e6bc23ca05b7431bf58
Thane Thomson <connect@thanethomson.com>
2020-11-02 08:50:30 -0500

    Allow for customization of run script params
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- rpc-probe/run-with-docker.sh
+++ rpc-probe/run-with-docker.sh
@@ -2,7 +2,16 @@
 set -e
 
 TENDERMINT_TAG=${TENDERMINT_TAG:-latest}
+DOCKER_PULL=${DOCKER_PULL:-1}
+VERBOSE=${VERBOSE:-0}
+OUTPUT=${OUTPUT:-"probe-results"}
+REQUEST_WAIT=${REQUEST_WAIT:-1000}
+
 TENDERMINT_IMAGE="tendermint/tendermint:${TENDERMINT_TAG}"
+FLAGS="--output ${OUTPUT} --request-wait ${REQUEST_WAIT}"
+if [ "${VERBOSE}" -eq "1" ]; then
+  FLAGS="${FLAGS} -v"
+fi
 
 echo "Using Tendermint Docker image: ${TENDERMINT_IMAGE}"
 
@@ -10,7 +19,13 @@ cargo build
 
 rm -rf /tmp/tendermint
 mkdir -p /tmp/tendermint
-docker pull "${TENDERMINT_IMAGE}"
+
+if [ "${DOCKER_PULL}" -eq "1" ]; then
+  docker pull "${TENDERMINT_IMAGE}"
+else
+  echo "Skipping pulling of Docker image"
+fi
+
 docker run -it --rm -v "/tmp/tendermint:/tendermint" "${TENDERMINT_IMAGE}" init
 docker run -d \
   --name rpc-probe-tendermint \
@@ -23,6 +38,6 @@ echo "Waiting for local Docker node to come up..."
 sleep 5
 
 set +e
-cargo run
+cargo run -- ${FLAGS}
 
 docker stop rpc-probe-tendermint
