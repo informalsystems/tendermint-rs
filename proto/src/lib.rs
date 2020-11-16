a38c3c30505d41f9a8e340d4092675617a6eb8d5
Thane Thomson <connect@thanethomson.com>
2020-11-16 09:36:54 -0500

    Clarify docs for Protobuf trait methods
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -167,7 +167,8 @@ where
 {
     /// Encode into a buffer in Protobuf format.
     ///
-    /// Equivalent to [`prost::Message::encode`].
+    /// Uses [`prost::Message::encode`] after converting into its counterpart
+    /// Protobuf data structure.
     ///
     /// [`prost::Message::encode`]: https://docs.rs/prost/*/prost/trait.Message.html#method.encode
     fn encode<B: BufMut>(&self, buf: &mut B) -> Result<(), Error> {
@@ -180,7 +181,8 @@ where
     ///
     /// An error will be returned if the buffer does not have sufficient capacity.
     ///
-    /// Equivalent to [`prost::Message::encode_length_delimited`].
+    /// Uses [`prost::Message::encode_length_delimited`] after converting into
+    /// its counterpart Protobuf data structure.
     ///
     /// [`prost::Message::encode_length_delimited`]: https://docs.rs/prost/*/prost/trait.Message.html#method.encode_length_delimited
     fn encode_length_delimited<B: BufMut>(&self, buf: &mut B) -> Result<(), Error> {
@@ -193,7 +195,7 @@ where
     ///
     /// The entire buffer will be consumed.
     ///
-    /// Equivalent to [`prost::Message::decode`] but with additional validation
+    /// Similar to [`prost::Message::decode`] but with additional validation
     /// prior to constructing the destination type.
     ///
     /// [`prost::Message::decode`]: https://docs.rs/prost/*/prost/trait.Message.html#method.decode
@@ -209,7 +211,7 @@ where
     ///
     /// The entire buffer will be consumed.
     ///
-    /// Equivalent to [`prost::Message::decode_length_delimited`] but with
+    /// Similar to [`prost::Message::decode_length_delimited`] but with
     /// additional validation prior to constructing the destination type.
     ///
     /// [`prost::Message::decode_length_delimited`]: https://docs.rs/prost/*/prost/trait.Message.html#method.decode_length_delimited
@@ -222,7 +224,8 @@ where
 
     /// Returns the encoded length of the message without a length delimiter.
     ///
-    /// Equivalent to [`prost::Message::encoded_len`].
+    /// Uses [`prost::Message::encoded_len`] after converting to its
+    /// counterpart Protobuf data structure.
     ///
     /// [`prost::Message::encoded_len`]: https://docs.rs/prost/*/prost/trait.Message.html#method.encoded_len
     fn encoded_len(&self) -> usize {
