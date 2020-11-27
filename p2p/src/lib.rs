ab81ed2be253ef9da658066fe5a9aa95a536851d
Anton Kaliaev <anton.kalyaev@gmail.com>
2020-11-27 15:00:53 +0400

    p2p: migrate secret_connection from tmkms (#696)
    * p2p: migrate secret_connection from tmkms
    
    Closes #691
    
    * bring back versions, remove generate_key
    
    * return proper error
    
    * format code
    
    * fixes after xla review
    
    * fixes after @tony-iqlusion review
    
    * specify tendermint and tendermint-proto as path dependencies
    
    * more fixes after @xla review
    
    * remove warnings from lib.rs
    
    https://github.com/melekes/tendermint-rs/commit/9e253dfde60e15b31b029dd07969c31cd6cc4a4d#r44484475
    
    * make amino dependencies optional

---
+++ p2p/src/lib.rs
@@ -0,0 +1,18 @@
+//! The Tendermint P2P stack.
+
+#![forbid(unsafe_code)]
+#![deny(
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_import_braces,
+    unused_qualifications,
+    rust_2018_idioms,
+    nonstandard_style
+)]
+#![doc(
+    html_root_url = "https://docs.rs/tendermint-p2p/0.1.0",
+    html_logo_url = "https://raw.githubusercontent.com/informalsystems/tendermint-rs/master/img/logo-tendermint-rs_3961x4001.png"
+)]
+
+pub mod error;
+pub mod secret_connection;
