316c6e568775b14802a89e683c39ae43787bb931
Thane Thomson <connect@thanethomson.com>
2020-12-01 12:45:12 -0500

    Add base64 en/decoding for key and value fields of tag struct
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- tendermint/src/abci/tag.rs
+++ tendermint/src/abci/tag.rs
@@ -16,7 +16,7 @@ pub struct Tag {
 
 /// Tag keys
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, PartialOrd, Ord, Serialize)]
-pub struct Key(String);
+pub struct Key(#[serde(with = "base64string")] String);
 
 impl AsRef<str> for Key {
     fn as_ref(&self) -> &str {
@@ -40,7 +40,7 @@ impl fmt::Display for Key {
 
 /// Tag values
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
-pub struct Value(String);
+pub struct Value(#[serde(with = "base64string")] String);
 
 impl AsRef<str> for Value {
     fn as_ref(&self) -> &str {
@@ -59,5 +59,42 @@ impl FromStr for Value {
 impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", &self.0)
+    }
+}
+
+mod base64string {
+    use serde::{Deserialize, Deserializer, Serializer};
+    use subtle_encoding::base64;
+
+    #[allow(clippy::ptr_arg)]
+    pub fn serialize<S>(value: &String, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let base64_bytes = base64::encode(value.as_bytes());
+        let base64_string = String::from_utf8(base64_bytes).map_err(serde::ser::Error::custom)?;
+        serializer.serialize_str(&base64_string)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<String, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = Option::<String>::deserialize(deserializer)?.unwrap_or_default();
+        String::from_utf8(base64::decode(&s).map_err(serde::de::Error::custom)?)
+            .map_err(serde::de::Error::custom)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn tag_serde() {
+        let json = r#"{"key": "cGFja2V0X3RpbWVvdXRfaGVpZ2h0", "value": "MC00ODQw"}"#;
+        let tag: Tag = serde_json::from_str(&json).unwrap();
+        assert_eq!("packet_timeout_height", tag.key.0);
+        assert_eq!("0-4840", tag.value.0);
     }
 }
