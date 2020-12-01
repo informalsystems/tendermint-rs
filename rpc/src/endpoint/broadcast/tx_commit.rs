29279c8f81de33b57495a0acc2afcc51433c6045
Thane Thomson <connect@thanethomson.com>
2020-12-01 12:45:49 -0500

    Extend TxResult from RPC to cover full range of returned fields for broadcast_tx_commit
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- rpc/src/endpoint/broadcast/tx_commit.rs
+++ rpc/src/endpoint/broadcast/tx_commit.rs
@@ -3,6 +3,8 @@
 
 use serde::{Deserialize, Serialize};
 
+use tendermint::abci::responses::Codespace;
+use tendermint::abci::{Event, Gas, Info};
 use tendermint::{
     abci::{transaction, Code, Data, Log, Transaction},
     block,
@@ -65,5 +67,26 @@ pub struct TxResult {
     pub data: Option<Data>,
 
     /// Log
+    #[serde(default)]
     pub log: Log,
+
+    /// ABCI info (nondeterministic)
+    #[serde(default)]
+    pub info: Info,
+
+    /// Amount of gas wanted
+    #[serde(default, rename = "gasWanted")]
+    pub gas_wanted: Gas,
+
+    /// Amount of gas used
+    #[serde(default, rename = "gasUsed")]
+    pub gas_used: Gas,
+
+    /// Events
+    #[serde(default)]
+    pub events: Vec<Event>,
+
+    /// Codespace
+    #[serde(default)]
+    pub codespace: Codespace,
 }
