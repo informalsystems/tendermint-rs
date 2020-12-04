550661fa479bd51205fb4429bb9accff0c262aad
Alexander Simmerl <a.simmerl@gmail.com>
2020-12-04 12:55:00 +0100

    Expand on peer needs and design

--- p2p/src/peer.rs
+++ p2p/src/peer.rs
@@ -2,6 +2,11 @@ use std::marker::PhantomData;
 
 use crate::transport::Connection;
 
+#[derive(Debug, thiserror::Error)]
+pub enum Error {}
+
+pub enum Message {}
+
 pub trait State {}
 pub enum Disconnected {}
 impl State for Disconnected {}
@@ -27,5 +32,28 @@ where
             connection,
             _state: PhantomData,
         }
+    }
+}
+
+impl<Conn> Iterator for Peer<Conn, Connected>
+where
+    Conn: Connection,
+{
+    type Item = Result<Message, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // TODO(xla): This is the place where we read bytes from the underlying connection and
+        // serialise them into `Message`s. If we can achieve that, that means no matter what
+        // connection is plugged we guarantee proper handling of wire protocol. In turn that means
+        // we assume the interface to be byte streams.
+        //
+        // Assumption here is that a unified event/message stream is wanted. An alternative model
+        // would be to have specialised streams where the messages are typed, similar to the idea
+        // of channelIded packets in the existing MConn.
+        //
+        // To decide what the right surface area is, there needs to be some exploration into the
+        // upper layer of peer management and coordination (Supervisor?). Which should inform if
+        // there is a need for finer grained control.
+        None
     }
 }
