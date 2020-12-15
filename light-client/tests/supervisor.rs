f094d98376735fc6c26d59eeef35cee377f2adaf
Shivani Joshi <joshi.shivani912@gmail.com>
2020-12-15 15:29:58 +0530

    replacing AnonLightBlock with LightBlock

--- light-client/tests/supervisor.rs
+++ light-client/tests/supervisor.rs
@@ -54,8 +54,7 @@ fn make_instance(peer_id: PeerId, trust_options: TrustOptions, io: MockIo, now: 
     Instance::new(light_client, state)
 }
 
-fn run_multipeer_test(tc: TestBisection<AnonLightBlock>) {
-    let tc: TestBisection<LightBlock> = tc.into();
+fn run_multipeer_test(tc: TestBisection<LightBlock>) {
     let primary = tc.primary.lite_blocks[0].provider;
 
     println!(
