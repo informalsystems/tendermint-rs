2381dcf8514a84c46532deae85aa3d7a5f586f64
Romain Ruetschi <romain@informal.systems>
2021-01-11 12:32:58 +0100

    Feature-guard the sled light store and hide the dependency on sled (#772)
    * Feature-guard the sled-based lightstore
    
    * Hide sled dependency by introducing SledStore::open
    
    * Enable lightstore-sled feature by default
    
    * Update changelog

--- light-client/src/store.rs
+++ light-client/src/store.rs
@@ -1,6 +1,7 @@
 //! Interface and implementations of the light block store.
 //!
 //! See the `memory` and `sled` modules for:
+//!
 //! - a transient, in-memory implementation for testing purposes
 //! - a persistent, on-disk, sled-backed implementation for production
 
@@ -10,6 +11,9 @@ use crate::types::{Height, LightBlock, Status};
 use crate::utils::std_ext;
 
 pub mod memory;
+
+#[cfg(feature = "lightstore-sled")]
+#[cfg_attr(docsrs, doc(cfg(feature = "lightstore-sled")))]
 pub mod sled;
 
 /// Store for light blocks.
