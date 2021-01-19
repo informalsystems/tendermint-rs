4a25f89d006d4642f31f9aaff99f3f43a688ec3d
Shivani Joshi <joshi.shivani912@gmail.com>
2021-01-19 21:18:52 +0530

    initial UT structure

--- light-client/src/supervisor.rs
+++ light-client/src/supervisor.rs
@@ -421,3 +421,130 @@ impl Handle for SupervisorHandle {
         Ok(receiver.recv().map_err(ErrorKind::from)?)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        components::{
+            io::{AtHeight, Io},
+            scheduler,
+            verifier::ProdVerifier,
+        },
+        fork_detector::ProdForkDetector,
+        light_client,
+        store::{memory::MemoryStore, LightStore},
+        tests::{MockClock, MockEvidenceReporter, MockIo, TrustOptions},
+        types::Time,
+    };
+    use std::{collections::HashMap, time::Duration};
+    use tendermint::block::Height;
+    use tendermint_testgen::LightChain;
+
+    pub struct TestBisection {
+        pub trust_options: TrustOptions,
+        pub primary: LightChain,
+        pub witnesses: Vec<LightChain>,
+        pub height_to_verify: Height,
+        pub now: Time,
+        pub expected_output: Option<String>,
+    }
+
+    fn make_instance(
+        peer_id: PeerId,
+        trust_options: TrustOptions,
+        io: MockIo,
+        now: Time,
+    ) -> Instance {
+        let trusted_height = trust_options.height;
+        let trusted_state = io
+            .fetch_light_block(AtHeight::At(trusted_height))
+            .expect("could not 'request' light block");
+
+        let mut light_store = MemoryStore::new();
+        light_store.insert(trusted_state, Status::Trusted);
+
+        let state = State {
+            light_store: Box::new(light_store),
+            verification_trace: HashMap::new(),
+        };
+
+        let options = light_client::Options {
+            trust_threshold: trust_options.trust_level,
+            trusting_period: trust_options.period.into(),
+            clock_drift: Duration::from_secs(10),
+        };
+
+        let verifier = ProdVerifier::default();
+        let clock = MockClock { now };
+        let scheduler = scheduler::basic_bisecting_schedule;
+
+        let light_client = LightClient::new(peer_id, options, clock, scheduler, verifier, io);
+
+        Instance::new(light_client, state)
+    }
+
+    fn run_multipeer_test(tc: TestBisection) {
+        let primary = tc.primary.lite_blocks[0].provider;
+
+        println!(
+            "Running Test Case: {}\nwith Primary Peer: {:?}",
+            tc.description, primary
+        );
+
+        let expects_err = match &tc.expected_output {
+            Some(eo) => eo.eq("error"),
+            None => false,
+        };
+
+        let io = MockIo::new(tc.primary.chain_id, tc.primary.lite_blocks);
+        let primary_instance = make_instance(primary, tc.trust_options.clone(), io.clone(), tc.now);
+
+        let mut peer_list = PeerList::builder();
+        peer_list.primary(primary, primary_instance);
+
+        for provider in tc.witnesses.into_iter() {
+            let peer_id = provider.value.lite_blocks[0].provider;
+            println!("Witness: {}", peer_id);
+            let io = MockIo::new(provider.value.chain_id, provider.value.lite_blocks);
+            let instance = make_instance(peer_id, tc.trust_options.clone(), io.clone(), tc.now);
+            peer_list.witness(peer_id, instance);
+        }
+
+        let supervisor = Supervisor::new(
+            peer_list.build(),
+            ProdForkDetector::default(),
+            MockEvidenceReporter::new(),
+        );
+
+        // TODO: Add method to `Handle` to get a copy of the current peer list
+
+        let handle = supervisor.handle();
+        std::thread::spawn(|| supervisor.run());
+
+        let target_height = tc.height_to_verify;
+
+        match handle.verify_to_target(target_height) {
+            Ok(new_state) => {
+                // Check that the expected state and new_state match
+                let untrusted_light_block = io
+                    .fetch_light_block(AtHeight::At(target_height))
+                    .expect("header at untrusted height not found");
+
+                let expected_state = untrusted_light_block;
+                assert_eq!(new_state.height(), expected_state.height());
+                assert_eq!(new_state, expected_state);
+
+                // Check the verdict
+                assert!(!expects_err);
+            }
+            Err(e) => {
+                dbg!(e);
+                assert!(expects_err);
+            }
+        }
+
+        // TODO: Check the peer list
+        // TODO: Check we recorded a fork evidence (or not)
+    }
+}
