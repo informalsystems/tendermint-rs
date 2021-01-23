4383f267503e44d8b5805309a6069b9ffa6269ab
Thane Thomson <connect@thanethomson.com>
2021-01-23 17:42:42 -0500

    Restructure errors and remove redundant ones
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- abci/src/result.rs
+++ abci/src/result.rs
@@ -8,30 +8,24 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// The various errors produced by the ABCI crate.
 #[derive(Debug, Error)]
 pub enum Error {
+    #[error("Tendermint error")]
+    TendermintError(#[from] tendermint::Error),
+
     #[error("protocol buffers error")]
     Protobuf(#[from] tendermint_proto::Error),
 
     #[error("network I/O error")]
     NetworkIo(#[from] std::io::Error),
 
-    #[cfg(any(feature = "runtime-tokio", feature = "runtime-async-std"))]
     #[error("channel send error: {0}")]
     ChannelSend(String),
 
-    #[cfg(feature = "runtime-tokio")]
-    #[error("failed to obtain UNIX stream path")]
-    CannotObtainUnixStreamPath,
-
-    #[error("Tendermint error")]
-    TendermintError(#[from] tendermint::Error),
-
-    #[error("server stream terminated unexpectedly")]
-    ServerStreamTerminated,
+    #[error("failed to receive message from channel: {0}")]
+    ChannelRecv(String),
 
     #[error("sending end of channel closed unexpectedly")]
     ChannelSenderClosed,
 
-    #[cfg(feature = "runtime-async-std")]
-    #[error("failed to receive message from channel: {0}")]
-    ChannelRecv(String),
+    #[error("server stream terminated unexpectedly")]
+    ServerStreamTerminated,
 }
