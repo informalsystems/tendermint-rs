a30d19d699193c2cc8941d8c2a3aa41a017671a8
Thane Thomson <connect@thanethomson.com>
2021-02-18 14:43:20 -0500

    Move ABCI server example to crate root
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- abci/src/lib.rs
+++ abci/src/lib.rs
@@ -1,4 +1,33 @@
 //! ABCI framework for building Tendermint applications in Rust.
+//!
+//! ## Example
+//!
+//! The following example shows how to use the trivial [`EchoApp`] to
+//! instantiate an ABCI server.
+//!
+//! **NOTE**: `EchoApp` won't work when coupled with a real Tendermint node, as
+//! it does not implement the minimum ABCI application functionality required
+//! by Tendermint. See the [`KeyValueStoreApp`] for a more functional ABCI
+//! application.
+//!
+//! ```rust
+//! use tendermint_abci::{EchoApp, ServerBuilder, ClientBuilder};
+//! use tendermint_proto::abci::RequestEcho;
+//!
+//! let server = ServerBuilder::default()
+//!     .bind("127.0.0.1:26658", EchoApp::default())
+//!     .unwrap();
+//! let server_addr = server.local_addr();
+//! std::thread::spawn(move || server.listen().unwrap());
+//!
+//! let mut client = ClientBuilder::default()
+//!     .connect(server_addr)
+//!     .unwrap();
+//!
+//! let message = String::from("Hello ABCI!");
+//! let response = client.echo(RequestEcho { message: message.clone() }).unwrap();
+//! assert_eq!(response.message, message);
+//! ```
 
 mod application;
 #[cfg(feature = "client")]
--- abci/src/server.rs
+++ abci/src/server.rs
@@ -12,27 +12,6 @@ use tracing::{error, info};
 pub const DEFAULT_SERVER_READ_BUF_SIZE: usize = 1024 * 1024;
 
 /// Allows us to configure and construct an ABCI server.
-///
-/// ## Example
-///
-/// ```rust
-/// use tendermint_abci::{EchoApp, ServerBuilder, ClientBuilder};
-/// use tendermint_proto::abci::RequestEcho;
-///
-/// let server = ServerBuilder::default()
-///     .bind("127.0.0.1:26658", EchoApp::default())
-///     .unwrap();
-/// let server_addr = server.local_addr();
-/// std::thread::spawn(move || server.listen().unwrap());
-///
-/// let mut client = ClientBuilder::default()
-///     .connect(server_addr)
-///     .unwrap();
-///
-/// let message = String::from("Hello ABCI!");
-/// let response = client.echo(RequestEcho { message: message.clone() }).unwrap();
-/// assert_eq!(response.message, message);
-/// ```
 pub struct ServerBuilder {
     read_buf_size: usize,
 }
