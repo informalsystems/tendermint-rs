a0a59b3a3f8a50abdaa618ff00394eeeeb8b9a0f
Shon Feder <shon@informal.systems>
2021-03-21 20:00:12 -0400

    Move time proptest generators into their own crate (#829)
    * Move time proptest generators into their own crate
    
    Closes #828
    
    This allows us to use the timestamp and DateTime generators in any other
    crate in our workspace. It should help advance #822 and #821.
    
    * Fix clippy warning
    
    * Correct documentation example
    
    * Update pbt-gen/src/time.rs
    
    Co-authored-by: Thane Thomson <thane@informal.systems>
    
    * Update pbt-gen/src/time.rs
    
    Co-authored-by: Thane Thomson <thane@informal.systems>
    
    * Fix typos in pbt-gen/Cargo.toml
    
    * Rename pbt-gen crate and guard modules with feature
    
    * Correct wording
    
    Co-authored-by: Thane Thomson <thane@informal.systems>

---
+++ pbt-gen/src/lib.rs
@@ -0,0 +1,19 @@
+//! This internal crate provides [proptest](https://github.com/AltSysrq/proptest)
+//! strategies and other PBT utilities used for testing.
+//!
+//! Conditions for inclusion in this crate are:
+//!
+//! 1. The utilities are relatively general.
+//! 2. The utilities don't rely on any code internal to the other crates of
+//! this repository.
+//!
+//! The each module of this crate (and the module's dependencies) are guarded by
+//! a feature, documented along with the module.
+//!
+//! The default features are:
+//!
+//! - "time"
+
+/// Enabled with the "time" feature:
+#[cfg(feature = "time")]
+pub mod time;
