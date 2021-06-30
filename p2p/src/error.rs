0df6c59eaf36d39e537e0c6899040c38ecddddad
Soares Chen <soares.chen@gmail.com>
2021-06-30 16:11:23 +0200

    Use flex-error for p2p

--- p2p/src/error.rs
+++ p2p/src/error.rs
@@ -1,19 +1,62 @@
 //! Error types
 
-use thiserror::Error;
+use flex_error::{define_error, DisplayError, TraceError};
+use prost::DecodeError;
+use prost_amino::DecodeError as AminoDecodeError;
+use signature::Error as SignatureError;
 
-/// Kinds of errors
-#[derive(Copy, Clone, Debug, Error, Eq, PartialEq)]
-pub enum Error {
-    /// Cryptographic operation failed
-    #[error("cryptographic error")]
-    Crypto,
+define_error! {
+    Error {
+        Crypto
+            | _ | { "cryptographic error" },
 
-    /// Malformatted or otherwise invalid cryptographic key
-    #[error("invalid key")]
-    InvalidKey,
+        InvalidKey
+            | _ | { "invalid key" },
 
-    /// Network protocol-related errors
-    #[error("protocol error")]
-    Protocol,
+        LowOrderKey
+            | _ | { "low-order points found (potential MitM attack!)" },
+
+        Protocol
+            | _ | { "protocol error" },
+
+        MalformedHandshake
+            | _ | { "malformed handshake message (protocol version mismatch?)" },
+
+        Io
+            [ TraceError<std::io::Error> ]
+            | _ | { "io error" },
+
+        Decode
+            [ TraceError<DecodeError> ]
+            | _ | { "malformed handshake message (protocol version mismatch?)" },
+
+        AminoDecode
+            [ TraceError<AminoDecodeError> ]
+            | _ | { "malformed handshake message (protocol version mismatch?)" },
+
+        MissingSecret
+            | _ | { "missing secret: forgot to call Handshake::new?" },
+
+        MissingKey
+            | _ | { "public key missing" },
+
+        Signature
+            [ TraceError<SignatureError> ]
+            | _ | { "signature error" },
+
+        UnsupportedKey
+            | _ | { "secp256k1 is not supported" },
+
+        Aead
+            [ DisplayError<aead::Error> ]
+            | _ | { "aead error" },
+
+        ShortCiphertext
+            { tag_size: usize }
+            | e | { format_args!("ciphertext must be at least as long as a MAC tag {}", e.tag_size) },
+
+        SmallOutputBuffer
+            | _ | { "output buffer is too small" },
+
+    }
 }
