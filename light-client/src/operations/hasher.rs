5b0ebc11932ce3be3815f8bdc6668a54b7383d92
Thane Thomson <connect@thanethomson.com>
2021-08-26 19:35:20 -0400

    Reduce to default hashing implementations
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- light-client/src/operations/hasher.rs
+++ light-client/src/operations/hasher.rs
@@ -2,34 +2,23 @@
 
 use crate::types::{Header, ValidatorSet};
 
-use tendermint::{merkle, Hash};
+use tendermint::Hash;
 
 /// Hashing for headers and validator sets
 pub trait Hasher: Send + Sync {
     /// Hash the given header
-    fn hash_header(&self, header: &Header) -> Hash;
+    fn hash_header(&self, header: &Header) -> Hash {
+        header.hash()
+    }
 
     /// Hash the given validator set
-    fn hash_validator_set(&self, validator_set: &ValidatorSet) -> Hash;
+    fn hash_validator_set(&self, validator_set: &ValidatorSet) -> Hash {
+        validator_set.hash()
+    }
 }
 
 /// Default implementation of a hasher
 #[derive(Clone, Copy, Debug, Default)]
 pub struct ProdHasher;
 
-impl Hasher for ProdHasher {
-    fn hash_header(&self, header: &Header) -> Hash {
-        header.hash()
-    }
-
-    /// Compute the Merkle root of the validator set
-    fn hash_validator_set(&self, validator_set: &ValidatorSet) -> Hash {
-        let validator_bytes: Vec<Vec<u8>> = validator_set
-            .validators()
-            .iter()
-            .map(|validator| validator.hash_bytes())
-            .collect();
-
-        Hash::Sha256(merkle::simple_hash_from_byte_vectors(validator_bytes))
-    }
-}
+impl Hasher for ProdHasher {}
