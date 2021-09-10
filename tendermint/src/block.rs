89e665984f33f0a97355edb0b56a68d406d3966e
Henry de Valence <hdevalence@hdevalence.ca>
2021-09-09 17:58:32 -0700

    tendermint: define Serde for Block in terms of RawBlock
    This changes the Serialize/Deserialize implementations for Block to convert
    to/from the proto-generated `RawBlock`, and use the derived serialization for
    that type.
    
    This is much cleaner and more maintainable than keeping Serde annotations for
    each sub-member of the data structure, because all of the serialization code is
    kept in one place, and there's only one validation path (the TryFrom conversion
    from RawBlock to Block) that applies to both kinds of serialization.

--- tendermint/src/block.rs
+++ tendermint/src/block.rs
@@ -35,6 +35,7 @@ use tendermint_proto::Protobuf;
 // Default serialization - all fields serialize; used by /block endpoint
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[non_exhaustive]
+#[serde(try_from = "RawBlock", into = "RawBlock")]
 pub struct Block {
     /// Block header
     pub header: Header,
@@ -46,7 +47,6 @@ pub struct Block {
     pub evidence: evidence::Data,
 
     /// Last commit
-    #[serde(with = "crate::serializers::optional")]
     pub last_commit: Option<Commit>,
 }
 
