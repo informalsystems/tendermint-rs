d0d0c5bbf2b785efbcd0cb9f926fbf08e2d831be
Tony Arcieri <tony@iqlusion.io>
2021-09-17 10:14:03 -0600

    p2p: fix clippy nits

--- p2p/src/secret_connection/amino_types.rs
+++ p2p/src/secret_connection/amino_types.rs
@@ -6,8 +6,8 @@ use ed25519_dalek as ed25519;
 use prost_derive::Message;
 use tendermint_proto as proto;
 
-/// Amino prefix for "tendermint/PubKeyEd25519"
-const PUB_KEY_ED25519_AMINO_PREFIX: [u8; 4] = [0x16, 0x24, 0xde, 0x64];
+/// Amino prefix for "tendermint/PubKeyEd25519" (4-bytes) + length prefix (1-byte)
+const PUB_KEY_ED25519_AMINO_PREFIX: [u8; 5] = [0x16, 0x24, 0xde, 0x64, 0x20];
 
 /// Authentication signature message
 #[derive(Clone, PartialEq, Message)]
@@ -24,8 +24,8 @@ pub struct AuthSigMessage {
 impl AuthSigMessage {
     pub fn new(pub_key: &ed25519::PublicKey, sig: &ed25519::Signature) -> Self {
         let mut pub_key_bytes = Vec::from(PUB_KEY_ED25519_AMINO_PREFIX);
-        pub_key_bytes.push(pub_key.as_ref().len() as u8);
         pub_key_bytes.extend_from_slice(pub_key.as_ref());
+
         Self {
             pub_key: pub_key_bytes,
             sig: sig.as_ref().to_vec(),
@@ -36,7 +36,7 @@ impl AuthSigMessage {
 impl TryFrom<AuthSigMessage> for tendermint_proto::p2p::AuthSigMessage {
     type Error = Error;
 
-    fn try_from(amino_msg: AuthSigMessage) -> Result<tendermint_proto::p2p::AuthSigMessage, Error> {
+    fn try_from(amino_msg: AuthSigMessage) -> Result<Self, Error> {
         // Strip Amino prefix
         if amino_msg.pub_key.len() < 5 {
             return Err(Error::protocol());
@@ -44,9 +44,7 @@ impl TryFrom<AuthSigMessage> for tendermint_proto::p2p::AuthSigMessage {
 
         let (amino_prefix, pub_key) = amino_msg.pub_key.split_at(5);
 
-        if amino_prefix[..4] != PUB_KEY_ED25519_AMINO_PREFIX
-            || amino_prefix[4] != ed25519::PUBLIC_KEY_LENGTH as u8
-        {
+        if amino_prefix != PUB_KEY_ED25519_AMINO_PREFIX {
             return Err(Error::protocol());
         }
 
@@ -54,7 +52,7 @@ impl TryFrom<AuthSigMessage> for tendermint_proto::p2p::AuthSigMessage {
             sum: Some(proto::crypto::public_key::Sum::Ed25519(pub_key.to_vec())),
         };
 
-        Ok(proto::p2p::AuthSigMessage {
+        Ok(Self {
             pub_key: Some(pub_key),
             sig: amino_msg.sig,
         })
