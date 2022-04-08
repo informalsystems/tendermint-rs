706ef1f13034da2d3904b0464bc89f360e03b065
xla <self@xla.is>
2022-04-08 15:25:10 +1000

    tools: Fix rpc-probe
    Signed-off-by: xla <self@xla.is>

--- tools/rpc-probe/src/kvstore.rs
+++ tools/rpc-probe/src/kvstore.rs
@@ -21,7 +21,8 @@ pub fn quick_probe_plan(output_path: &Path, request_wait: Duration) -> Result<Pl
                     .with_min_height(10)
                     .with_name("block_at_height_10"),
                 block_results(10).with_name("block_results_at_height_10"),
-                block_by_hash("0x00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF").with_name("block_by_hash"),
+                block_by_hash("0x00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF")
+                    .with_name("block_by_hash"),
                 block_search("block.height > 1", 1, 10, "asc").with_name("block_search"),
                 blockchain(1, 10).with_name("blockchain_from_1_to_10"),
                 commit(10).with_name("commit_at_height_10"),
@@ -70,122 +71,5 @@ pub fn quick_probe_plan(output_path: &Path, request_wait: Duration) -> Result<Pl
             ]),
         ],
     )
-<<<<<<< HEAD
-=======
     .into()
-}
-
-pub fn block(height: u64) -> PlannedInteraction {
-    Request::new(
-        "block",
-        json!({
-            "height": format!("{}", height),
-        }),
-    )
-    .into()
-}
-
-pub fn block_search(query: &str, page: u32, per_page: u32, order_by: &str) -> PlannedInteraction {
-    Request::new(
-        "block_search",
-        json!({
-            "query": query,
-            "page": format!("{}", page),
-            "per_page": format!("{}", per_page),
-            "order_by": order_by,
-        }),
-    )
-    .into()
-}
-
-pub fn block_results(height: u64) -> PlannedInteraction {
-    Request::new(
-        "block_results",
-        json!({
-            "height": format!("{}", height),
-        }),
-    )
-    .into()
-}
-
-pub fn blockchain(min_height: u64, max_height: u64) -> PlannedInteraction {
-    Request::new(
-        "blockchain",
-        json!({
-            "minHeight": format!("{}", min_height),
-            "maxHeight": format!("{}", max_height),
-        }),
-    )
-    .into()
-}
-
-pub fn broadcast_tx(method: &str, key: &str, value: &str) -> PlannedInteraction {
-    Request::new(
-        format!("broadcast_tx_{}", method).as_str(),
-        json!({
-            "tx": encode_kvpair(key, value),
-        }),
-    )
-    .into()
-}
-
-pub fn commit(height: u64) -> PlannedInteraction {
-    Request::new(
-        "commit",
-        json!({
-            "height": format!("{}", height),
-        }),
-    )
-    .into()
-}
-
-pub fn consensus_params(height: u64) -> PlannedInteraction {
-    Request::new(
-        "consensus_params",
-        json!({
-            "height": format!("{}", height),
-        }),
-    )
-    .into()
-}
-
-pub fn consensus_state() -> PlannedInteraction {
-    Request::new("consensus_state", json!(null)).into()
-}
-
-pub fn genesis() -> PlannedInteraction {
-    Request::new("genesis", json!(null)).into()
-}
-
-pub fn net_info() -> PlannedInteraction {
-    Request::new("net_info", json!(null)).into()
-}
-
-pub fn status() -> PlannedInteraction {
-    Request::new("status", json!(null)).into()
-}
-
-pub fn subscribe(query: &str) -> PlannedInteraction {
-    PlannedSubscription::new(query).into()
-}
-
-pub fn tx_search(
-    query: &str,
-    prove: bool,
-    page: u32,
-    per_page: u8,
-    order_by: &str,
-) -> PlannedInteraction {
-    Request::new(
-        "tx_search",
-        json!({
-            "query": query,
-            "prove": prove,
-            "page": format!("{}", page),
-            "per_page": format!("{}", per_page),
-            "order_by": order_by,
-        }),
-    )
-    .into()
->>>>>>> 38d91a59 (light-client: Replace Io trait with async variant)
 }
