c917544e9c03028ddf39c2009faf239f7ac67c47
Thane Thomson <thane@informal.systems>
2022-05-06 08:03:58 -0400

    Bump protos to those provided by Tendermint v0.35.4 (#1126)
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- proto/src/tendermint.rs
+++ proto/src/tendermint.rs
@@ -1,53 +1,15 @@
 //! Tendermint-proto auto-generated sub-modules for Tendermint
 
-pub mod statesync {
-    include!("prost/tendermint.statesync.rs");
-}
-
-pub mod abci {
-    include!("prost/tendermint.abci.rs");
-}
-
-pub mod version {
-    include!("prost/tendermint.version.rs");
+pub mod consensus {
+    include!("prost/tendermint.consensus.rs");
 }
 
 pub mod types {
     include!("prost/tendermint.types.rs");
 }
 
-pub mod consensus {
-    include!("prost/tendermint.consensus.rs");
-}
-
-pub mod p2p {
-    include!("prost/tendermint.p2p.rs");
-}
-
-pub mod privval {
-    include!("prost/tendermint.privval.rs");
-}
-
-pub mod crypto {
-    include!("prost/tendermint.crypto.rs");
-}
-
 pub mod mempool {
     include!("prost/tendermint.mempool.rs");
-}
-
-pub mod blocksync {
-    include!("prost/tendermint.blocksync.rs");
-}
-
-pub mod state {
-    include!("prost/tendermint.state.rs");
-}
-
-pub mod libs {
-    pub mod bits {
-        include!("prost/tendermint.libs.bits.rs");
-    }
 }
 
 pub mod rpc {
@@ -56,7 +18,45 @@ pub mod rpc {
     }
 }
 
+pub mod libs {
+    pub mod bits {
+        include!("prost/tendermint.libs.bits.rs");
+    }
+}
+
+pub mod state {
+    include!("prost/tendermint.state.rs");
+}
+
+pub mod version {
+    include!("prost/tendermint.version.rs");
+}
+
+pub mod blocksync {
+    include!("prost/tendermint.blocksync.rs");
+}
+
+pub mod privval {
+    include!("prost/tendermint.privval.rs");
+}
+
+pub mod statesync {
+    include!("prost/tendermint.statesync.rs");
+}
+
+pub mod p2p {
+    include!("prost/tendermint.p2p.rs");
+}
+
+pub mod abci {
+    include!("prost/tendermint.abci.rs");
+}
+
+pub mod crypto {
+    include!("prost/tendermint.crypto.rs");
+}
+
 pub mod meta {
     pub const REPOSITORY: &str = "https://github.com/tendermint/tendermint";
-    pub const COMMITISH: &str = "v0.35.0";
+    pub const COMMITISH: &str = "v0.35.4";
 }
