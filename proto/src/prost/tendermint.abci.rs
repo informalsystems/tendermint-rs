83872d05bc26d9571d4867c35065f9d189923fd7
Mikhail Zabaluev <mikhail@informal.systems>
2022-05-24 18:56:32 +0300

    Fix JSON deserialization of abci::ResponseInfo
    The fields are annotated with json:"omitempty" in the Go source,
    so give them default values for Deserialize. The /abci_info endpoint
    is known to omit the app_version field in some recent revisions of Gaia.

--- proto/src/prost/tendermint.abci.rs
+++ proto/src/prost/tendermint.abci.rs
@@ -216,15 +216,17 @@ pub struct ResponseFlush {
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ResponseInfo {
     #[prost(string, tag="1")]
+    #[serde(default)]
     pub data: ::prost::alloc::string::String,
     /// this is the software version of the application. TODO: remove?
     #[prost(string, tag="2")]
+    #[serde(default)]
     pub version: ::prost::alloc::string::String,
     #[prost(uint64, tag="3")]
-    #[serde(with = "crate::serializers::from_str")]
+    #[serde(with = "crate::serializers::from_str", default)]
     pub app_version: u64,
     #[prost(int64, tag="4")]
-    #[serde(with = "crate::serializers::from_str")]
+    #[serde(with = "crate::serializers::from_str", default)]
     pub last_block_height: i64,
     #[prost(bytes="bytes", tag="5")]
     #[serde(skip_serializing_if = "bytes::Bytes::is_empty")]
