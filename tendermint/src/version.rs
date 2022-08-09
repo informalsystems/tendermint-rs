956274091af679696864c6806c66c4b4034e9ba9
aaronwinter <erwan.ounn.84@gmail.com>
2022-08-09 15:58:58 +0300

    feat(tendermint/channels): rudimentary `TryFrom` for `Channels` and `Version`

--- tendermint/src/version.rs
+++ tendermint/src/version.rs
@@ -6,7 +6,15 @@ use crate::prelude::*;
 
 /// Tendermint version
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
-pub struct Version(String);
+pub struct Version(pub String);
+
+impl TryFrom<String> for Version {
+    type Error = crate::error::Error;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        // TODO(erwan): what validation does `Version` needs?
+        Ok(Version(value))
+    }
+}
 
 impl Display for Version {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
