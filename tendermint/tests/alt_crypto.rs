76a3fceddfcad1b226bad0db320c6f4e54e5dc19
Mikhail Zabaluev <mikhail@informal.systems>
2022-12-01 22:02:59 +0200

    Fix a clippy lint in alt_crypto test

--- tendermint/tests/alt_crypto.rs
+++ tendermint/tests/alt_crypto.rs
@@ -119,7 +119,7 @@ mod tests {
         hasher.update(b"hello world");
         let hash = hasher.finalize();
 
-        let hash = String::from_utf8(hex::encode(&hash)).unwrap();
+        let hash = String::from_utf8(hex::encode(hash)).unwrap();
         assert_eq!(
             hash,
             "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
