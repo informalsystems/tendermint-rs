2b49f633db75fa3e65a80a4c6c4286dbd593fa85
Mikhail Zabaluev <mikhail@informal.systems>
2023-01-24 20:40:55 +0200

    tendermint: rework signature in alt_crypto test
    Implement the new Verifier trait instead of the signature traits.

--- tendermint/tests/alt_crypto.rs
+++ tendermint/tests/alt_crypto.rs
@@ -1,70 +1,37 @@
 //! An imitation of alternative cryptographic function implementations
 //! for a chain environment that provides its own cryptographic API.
+#![cfg(all(feature = "secp256k1", feature = "rust-crypto"))]
 
-use tendermint::crypto::{sha256::HASH_SIZE, Sha256};
-
+use ::signature::{DigestVerifier, Signature as _};
 use digest::Digest;
-use signature::{self, DigestSigner, DigestVerifier, Signature, Signer, Verifier};
 
-use k256::ecdsa::{SigningKey, VerifyingKey};
+use tendermint::crypto::signature::{self, Verifier};
+use tendermint::crypto::{sha256::HASH_SIZE, Sha256};
+use tendermint::{PublicKey, Signature};
 
 #[derive(Debug, Default)]
 struct SubstrateSha256(sha2::Sha256);
 
-#[derive(Debug)]
-struct SubstrateSignature(k256::ecdsa::Signature);
+#[derive(Debug, Default)]
+struct SubstrateSignatureVerifier;
 
-impl AsRef<[u8]> for SubstrateSignature {
-    fn as_ref(&self) -> &[u8] {
-        self.0.as_bytes()
-    }
-}
-
-impl Signature for SubstrateSignature {
-    fn from_bytes(bytes: &[u8]) -> Result<Self, signature::Error> {
-        let inner = k256::ecdsa::Signature::from_bytes(bytes)?;
-        Ok(Self(inner))
-    }
-}
-
-struct SubstrateSigner {
-    inner: SigningKey,
-}
-
-impl SubstrateSigner {
-    fn from_bytes(private_key: &[u8]) -> Result<Self, signature::Error> {
-        let inner = SigningKey::from_bytes(private_key)?;
-        Ok(Self { inner })
-    }
-}
-
-impl Signer<SubstrateSignature> for SubstrateSigner {
-    fn try_sign(&self, msg: &[u8]) -> Result<SubstrateSignature, signature::Error> {
-        let mut hasher = sha2::Sha256::new();
-        hasher.update(msg);
-        let signature = self.inner.try_sign_digest(hasher)?;
-        Ok(SubstrateSignature(signature))
-    }
-}
-
-#[derive(Debug)]
-struct SubstrateSignatureVerifier {
-    inner: VerifyingKey,
-}
-
-impl SubstrateSignatureVerifier {
-    fn from_bytes(public_key: &[u8]) -> Result<Self, signature::Error> {
-        Ok(Self {
-            inner: VerifyingKey::from_sec1_bytes(public_key)?,
-        })
-    }
-}
-
-impl Verifier<SubstrateSignature> for SubstrateSignatureVerifier {
-    fn verify(&self, msg: &[u8], signature: &SubstrateSignature) -> Result<(), signature::Error> {
-        let mut hasher = sha2::Sha256::new();
-        Digest::update(&mut hasher, msg);
-        self.inner.verify_digest(hasher, &signature.0)
+impl Verifier for SubstrateSignatureVerifier {
+    fn verify(
+        pubkey: PublicKey,
+        msg: &[u8],
+        signature: &Signature,
+    ) -> Result<(), signature::Error> {
+        match pubkey {
+            PublicKey::Secp256k1(pk) => {
+                let sig = k256::ecdsa::Signature::from_bytes(signature.as_bytes())
+                    .map_err(|_| signature::Error::MalformedSignature)?;
+                let mut hasher = sha2::Sha256::new();
+                Digest::update(&mut hasher, msg);
+                pk.verify_digest(hasher, &sig)
+                    .map_err(|_| signature::Error::VerificationFailed)
+            },
+            _ => Err(signature::Error::UnsupportedKeyType),
+        }
     }
 }
 
@@ -75,9 +42,10 @@ impl Sha256 for SubstrateSha256 {
 }
 
 mod tests {
-    use super::{SubstrateSha256, SubstrateSignatureVerifier, SubstrateSigner};
-    use signature::{Signature, Signer, Verifier};
+    use super::{SubstrateSha256, SubstrateSignatureVerifier};
+    use tendermint::crypto::signature::Verifier;
     use tendermint::crypto::Sha256;
+    use tendermint::{PublicKey, Signature};
 
     use subtle_encoding::hex;
 
@@ -92,30 +60,19 @@ mod tests {
         );
     }
 
-    const SIGNING_KEY: &[u8] = b"59820654790d53a23d1017b50ddcdb31242e27c682a0a1372fc63c01dd48816a";
+    //const SIGNING_KEY: &[u8] = b"59820654790d53a23d1017b50ddcdb31242e27c682a0a1372fc63c01dd48816a";
     const VERIFYING_KEY: &[u8] =
         b"03cf7a110053a95b4b25266c3416ae342eba2ca3f4658fa1069fcf750f760b8c42";
     const MESSAGE: &[u8] = b"hello world";
     const SIGNATURE: &str = "684c3c183f76a79fc116dd4edd39fe40737cea51c6c1df47ff544c20d14a7a76754c43c51e0daa647e8e4164f254bb62dbf9bd5b2e2e03ffb8247dd92ce1e1e3";
 
     #[test]
-    fn signer_can_sign() {
-        let key_bytes = hex::decode(SIGNING_KEY).unwrap();
-
-        let signer = SubstrateSigner::from_bytes(&key_bytes).unwrap();
-        let signature = signer.sign(MESSAGE);
-
-        let sig_hex = String::from_utf8(hex::encode(signature.as_bytes())).unwrap();
-        assert_eq!(sig_hex, SIGNATURE);
-    }
-
-    #[test]
     fn verifier_can_verify() {
         let key_bytes = hex::decode(VERIFYING_KEY).unwrap();
-        let signature = hex::decode(SIGNATURE.as_bytes()).unwrap();
-        let signature = Signature::from_bytes(&signature).unwrap();
+        let public_key = PublicKey::from_raw_secp256k1(&key_bytes).unwrap();
+        let sig_bytes = hex::decode(SIGNATURE.as_bytes()).unwrap();
+        let signature = Signature::try_from(&sig_bytes[..]).unwrap();
 
-        let verifier = SubstrateSignatureVerifier::from_bytes(&key_bytes).unwrap();
-        verifier.verify(MESSAGE, &signature).unwrap();
+        SubstrateSignatureVerifier::verify(public_key, MESSAGE, &signature).unwrap();
     }
 }
