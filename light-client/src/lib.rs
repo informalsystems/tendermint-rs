212891e0b13c4d158c19ac2629efa939e852ae20
Romain Ruetschi <romain@informal.systems>
2023-03-08 10:20:12 +0100

    Remove `Supervisor` and `EvidenceReporter`, as well as associated examples and tests

--- light-client/src/lib.rs
+++ light-client/src/lib.rs
@@ -19,14 +19,10 @@ pub mod builder;
 pub mod components;
 pub mod contracts;
 pub mod errors;
-pub mod evidence;
-pub mod fork_detector;
 pub mod instance;
 pub mod light_client;
-pub mod peer_list;
 pub mod state;
 pub mod store;
-pub mod supervisor;
 
 pub(crate) mod utils;
 
