138ca642384f488c98388ce6dee4aa3b0ed15a8b
Romain Ruetschi <romain@informal.systems>
2023-03-10 18:14:33 +0100

    Work around detector bug

--- light-client/src/misbehavior/evidence.rs
+++ light-client/src/misbehavior/evidence.rs
@@ -34,7 +34,7 @@ pub fn make_evidence(
     );
 
     let witness = if conflicting_header_is_invalid {
-        common
+        Some(trusted).unwrap() // common
     } else {
         trusted
     };
