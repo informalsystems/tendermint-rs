eba40662c13d8bfeae73ef72d6af7cbe68df08d2
Mikhail Zabaluev <mikhail@informal.systems>
2023-04-24 15:37:03 +0300

    proto: add serializers for v0_38::types::Evidence

--- proto/src/serializers/evidence.rs
+++ proto/src/serializers/evidence.rs
@@ -45,3 +45,27 @@ mod v0_37 {
         }
     }
 }
+
+mod v0_38 {
+    use crate::v0_38::types::{evidence, Evidence};
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    impl<'de> Deserialize<'de> for Evidence {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let sum = Option::<evidence::Sum>::deserialize(deserializer)?;
+            Ok(Self { sum })
+        }
+    }
+
+    impl Serialize for Evidence {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            self.sum.serialize(serializer)
+        }
+    }
+}
