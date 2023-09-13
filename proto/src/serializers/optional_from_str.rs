185315e7743a16e75e936c639b52cc2733f80661
Mikhail Zabaluev <mikhail@informal.systems>
2023-09-13 13:08:39 +0300

    Test owned case in serializers::optional_from_str

--- proto/src/serializers/optional_from_str.rs
+++ proto/src/serializers/optional_from_str.rs
@@ -30,3 +30,35 @@ where
     };
     Ok(Some(s.parse().map_err(D::Error::custom)?))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
+    use core::convert::Infallible;
+    use core::str::FromStr;
+    use serde::Deserialize;
+
+    #[derive(Debug, PartialEq)]
+    struct ParsedStr(String);
+
+    impl FromStr for ParsedStr {
+        type Err = Infallible;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            Ok(Self(s.to_owned()))
+        }
+    }
+
+    #[derive(Deserialize)]
+    struct Foo {
+        #[serde(with = "super")]
+        msg: Option<ParsedStr>,
+    }
+
+    #[test]
+    fn can_deserialize_owned() {
+        const TEST_JSON: &str = r#"{ "msg": "\"Hello\"" }"#;
+        let v = serde_json::from_str::<Foo>(TEST_JSON).unwrap();
+        assert_eq!(v.msg, Some(ParsedStr("\"Hello\"".into())));
+    }
+}
