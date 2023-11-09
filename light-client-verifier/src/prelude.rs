23946a95f9927ad8e53369dc1e6e93b742fb4acd
Mikhail Zabaluev <mikhail@informal.systems>
2023-11-09 13:44:53 +0200

    Restore vec macro import in prelude
    Override the lint more selectively, specifically for this macro
    re-export.

--- light-client-verifier/src/prelude.rs
+++ light-client-verifier/src/prelude.rs
@@ -1,10 +1,10 @@
 // Re-export according to alloc::prelude::v1 because it is not yet stabilized
 // https://doc.rust-lang.org/src/alloc/prelude/v1.rs.html
 #[allow(unused_imports)]
+pub use alloc::vec;
 pub use alloc::{
     boxed::Box,
     string::{String, ToString},
-    vec,
     vec::Vec,
 };
 pub use core::prelude::v1::*;
--- proto/src/prelude.rs
+++ proto/src/prelude.rs
@@ -1,5 +1,8 @@
 // Re-export according to alloc::prelude::v1 because it is not yet stabilized
 // https://doc.rust-lang.org/src/alloc/prelude/v1.rs.html
+
+#[allow(unused_imports)]
+pub use alloc::vec;
 pub use alloc::{
     borrow::ToOwned,
     format,
