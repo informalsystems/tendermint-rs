58ff7662fccfc06787e72eadee38c09acc5c9ff1
Luca Joss <43531661+ljoss17@users.noreply.github.com>
2024-07-23 12:27:08 +0200

    rpc: Fix `AppHash` decoding failure in `/block_results` response (#1449)
    * If AppHash decoding fails, try with decoding with base64
    
    * Add changelog entry
    
    * Update .changelog/unreleased/improvements/1449-allow-base64-apphash.md
    
    Co-authored-by: Romain Ruetschi <romain@informal.systems>
    
    ---------
    
    Co-authored-by: Romain Ruetschi <romain@informal.systems>

--- tendermint/src/serializers/apphash.rs
+++ tendermint/src/serializers/apphash.rs
@@ -1,9 +1,10 @@
 //! AppHash serialization with validation
 
+use core::str::FromStr;
+
 use alloc::borrow::Cow;
 
-use serde::{de, ser, Deserialize, Deserializer, Serializer};
-use subtle_encoding::hex;
+use serde::{de, Deserialize, Deserializer, Serializer};
 
 use crate::{prelude::*, AppHash};
 
@@ -13,7 +14,7 @@ where
     D: Deserializer<'de>,
 {
     let hexstring = Option::<Cow<'_, str>>::deserialize(deserializer)?.unwrap_or(Cow::Borrowed(""));
-    AppHash::from_hex_upper(&hexstring).map_err(de::Error::custom)
+    AppHash::from_str(&hexstring).map_err(de::Error::custom)
 }
 
 /// Serialize from AppHash into hexstring
@@ -21,8 +22,6 @@ pub fn serialize<S>(value: &AppHash, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
-    let hex_bytes = hex::encode_upper(value.as_ref());
-    let hex_string = String::from_utf8(hex_bytes).map_err(ser::Error::custom)?;
     // Serialize as Option<String> for symmetry with deserialize
-    serializer.serialize_some(&hex_string)
+    serializer.serialize_some(&value.to_string())
 }
